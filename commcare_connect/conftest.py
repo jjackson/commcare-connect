6f64f6eec52516a92424e2b5d6f05102ccff4dde
Simon Kelly <skelly@dimagi.com>
2023-05-31 16:05:20 +0200

    initial

---
+++ commcare_connect/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from commcare_connect.users.models import User
+from commcare_connect.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
