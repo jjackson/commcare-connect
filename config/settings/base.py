74ae18d701114035642f087ff24134ed160396d6
Pawan Verma <pverma@dimagi.com>
2023-06-05 14:57:15 +0530

    Add app, models and migration for opportunity

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "commcare_connect.users",
+    "commcare_connect.opportunity"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
