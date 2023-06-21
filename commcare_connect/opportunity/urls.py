4acd3fabde0978561c8cfe3a67bc9e7cb7488b02
Pawan Verma <pverma@dimagi.com>
2023-06-21 15:15:25 +0530

    Add opportunity list to opportunity urls

--- commcare_connect/opportunity/urls.py
+++ commcare_connect/opportunity/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from commcare_connect.opportunity.views import OpportunityCreate, OpportunityEdit
+from commcare_connect.opportunity.views import OpportunityCreate, OpportunityEdit, OpportunityList
 
 app_name = "opportunity"
 urlpatterns = [
-    path("create/", view=OpportunityCreate.as_view(), name="opportunity_create"),
-    path("<int:pk>/", view=OpportunityEdit.as_view(), name="opportunity_edit"),
+    path("", view=OpportunityList.as_view(), name="list"),
+    path("create/", view=OpportunityCreate.as_view(), name="create"),
+    path("<int:pk>/", view=OpportunityEdit.as_view(), name="edit"),
 ]
--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,6 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
-from commcare_connect.opportunity.views import OpportunityList
-
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
@@ -18,7 +16,6 @@ urlpatterns = [
     path("users/", include("commcare_connect.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path("a/<slug:org_slug>/", view=OpportunityList.as_view(), name="opportunity_list"),
     path("a/<slug:org_slug>/opportunity/", include("commcare_connect.opportunity.urls", namespace="opportunity")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
