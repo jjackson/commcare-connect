85fd939345e3bdf0c863b0a56a1dd967c439764b
Pawan Verma <pverma@dimagi.com>
2023-06-21 11:27:43 +0530

    Add opportunity admin page

--- commcare_connect/opportunity/admin.py
+++ commcare_connect/opportunity/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
 
-from commcare_connect.opportunity.forms import OpportunityChangeForm, OpportunityCreationForm
 from commcare_connect.opportunity.models import Opportunity
 
 # Register your models here.
 
 
-@admin.register(Opportunity)
-class OpportunityAdmin(admin.ModelAdmin):
-    form = OpportunityChangeForm
-    add_form = OpportunityCreationForm
-
-    def get_form(self, request, obj=None, **kwargs):
-        defaults = {}
-        if obj is None:
-            defaults["form"] = self.add_form
-        defaults.update(kwargs)
-        return super().get_form(request, obj, **defaults)
+admin.site.register(Opportunity)
