ab9218deba06aa89df5d386e44eaf56a0e64f8bd
Pawan Verma <pverma@dimagi.com>
2023-06-26 16:18:17 +0530

    Add commcareapp model and migrations

--- commcare_connect/opportunity/models.py
+++ commcare_connect/opportunity/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-from commcare_connect.users.models import BaseModel, Organization
+from commcare_connect.users.models import Organization
+from commcare_connect.utils.db import BaseModel
+
+
+class CommCareApp(BaseModel):
+    cc_domain = models.CharField(max_length=255)
+    cc_app_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
 
 
 class Opportunity(BaseModel):
@@ -13,3 +21,14 @@ class Opportunity(BaseModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
     active = models.BooleanField(default=True)
+    learn_app = models.ForeignKey(
+        CommCareApp,
+        on_delete=models.CASCADE,
+        related_name="learn_app_opportunities",
+        null=True,
+    )
+    deliver_app = models.ForeignKey(
+        CommCareApp,
+        on_delete=models.CASCADE,
+        null=True,
+    )
