5893717617ba60aa145e86a248b2bac9996923cc
Pawan Verma <pverma@dimagi.com>
2023-06-26 16:22:05 +0530

    Refactor BaseModel to db utilities

--- commcare_connect/utils/db.py
+++ commcare_connect/utils/db.py
@@ -1,4 +1,15 @@
+from django.db import models
 from django.utils.text import slugify
+
+
+class BaseModel(models.Model):
+    created_by = models.CharField(max_length=255)
+    modified_by = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
 def slugify_uniquely(value, model, slugfield="slug"):
