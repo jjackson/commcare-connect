8c6d24579d9a5418abb7c0d85e609f82063f90f2
Simon Kelly <skelly@dimagi.com>
2023-07-25 10:27:24 +0200

    remove '~' from urls

--- commcare_connect/users/urls.py
+++ commcare_connect/users/urls.py
@@ -4,7 +4,7 @@ from commcare_connect.users.views import user_detail_view, user_redirect_view, u
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
+    path("redirect/", view=user_redirect_view, name="redirect"),
+    path("update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
 ]
