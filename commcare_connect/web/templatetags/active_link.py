cc9b7bc1708e9032bb0acc06e2423e38d14ac2d1
Simon Kelly <skelly@dimagi.com>
2023-07-25 11:45:13 +0200

    activate link for multiple views

--- commcare_connect/web/templatetags/active_link.py
+++ commcare_connect/web/templatetags/active_link.py
@@ -4,13 +4,14 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def active_link(context, viewnames, css_class='active', inactive_class='', *args, **kwargs):
+def active_link(context, viewnames, css_class='active', inactive_class='', namespace=None, *args, **kwargs):
     """
     Renders the given CSS class if the request path matches the path of the view.
     :param context: The context where the tag was called. Used to access the request object.
     :param viewnames: The name of the view or views separated by || (include namespaces if any).
     :param css_class: The CSS class to render.
     :param inactive_class: The CSS class to render if the views is not active.
+    :param namespace: The namespace of the view or views. This can also be provided in the view name.
     :return:
     """
     request = context.get('request')
@@ -22,11 +23,11 @@ def active_link(context, viewnames, css_class='active', inactive_class='', *args
     active = False
     views = viewnames.split('||')
     for view_name in views:
-        namespace = None
+        view_namespace = namespace
         if ":" in view_name:
-            namespace, view_name = view_name.split(":", 1)
+            view_namespace, view_name = view_name.split(":", 1)
 
-        if not namespace or namespace in namespaces:
+        if not view_namespace or view_namespace in namespaces:
             active = current_url_name == view_name.strip()
 
         if active:
