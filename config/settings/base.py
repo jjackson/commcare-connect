ccdafaceb68b851f4964399fe10b0e1b631f0496
Simon Kelly <skelly@dimagi.com>
2023-07-25 11:22:00 +0200

    add organization middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,6 +137,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "commcare_connect.users.middleware.OrganizationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
