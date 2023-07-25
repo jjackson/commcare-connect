0d0fc112497bf0c9261bfd2bd1a3b4cbd0a8109f
Simon Kelly <skelly@dimagi.com>
2023-07-25 15:57:04 +0200

    initial code to process assessments

--- commcare_connect/form_receiver/processor.py
+++ commcare_connect/form_receiver/processor.py
@@ -1,15 +1,30 @@
+from jsonpath_ng import JSONPathError
 from jsonpath_ng.ext import parse
 
 from commcare_connect.form_receiver.const import CCC_LEARN_XMLNS
+from commcare_connect.form_receiver.exceptions import ProcessingError
 
 LEARN_MODULE_JSONPATH = parse("module where @xmlns")
+ASSESSMENT_JSONPATH = parse("assessment where @xmlns")
 
 
 def process_xform(domain: str, app_id: str, form: dict):
-    modules = [match.value for match in LEARN_MODULE_JSONPATH.find(form) if match.value["@xmlns"] == CCC_LEARN_XMLNS]
-    if modules:
-        process_learn_modules(domain, app_id, modules)
+    processors = [
+        (LEARN_MODULE_JSONPATH, process_learn_modules),
+        (ASSESSMENT_JSONPATH, process_assessments),
+    ]
+    for jsonpath, processor in processors:
+        try:
+            matches = [match.value for match in jsonpath.find(form) if match.value["@xmlns"] == CCC_LEARN_XMLNS]
+            if matches:
+                processor(domain, app_id, matches)
+        except JSONPathError as e:
+            raise ProcessingError from e
 
 
 def process_learn_modules(domain, app_id, modules):
+    pass
+
+
+def process_assessments(domain, app_id, assessments):
     pass
