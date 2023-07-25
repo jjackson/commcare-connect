81ef5b0ab81cf1f4442f4f238587bec569c60531
Simon Kelly <skelly@dimagi.com>
2023-07-25 16:53:21 +0200

    don't use name as a filter for existing apps

--- commcare_connect/opportunity/forms.py
+++ commcare_connect/opportunity/forms.py
@@ -100,10 +100,10 @@ class OpportunityCreationForm(forms.ModelForm):
             if app["id"] == self.cleaned_data["learn_app"]:
                 self.instance.learn_app, _ = CommCareApp.objects.get_or_create(
                     cc_app_id=app["id"],
-                    name=app["name"],
                     cc_domain=app["domain"],
                     organization=organization,
                     defaults={
+                        "name": app["name"],
                         "created_by": self.user.email,
                         "modified_by": self.user.email,
                         "description": self.cleaned_data["learn_app_description"],
@@ -114,10 +114,10 @@ class OpportunityCreationForm(forms.ModelForm):
             if app["id"] == self.cleaned_data["deliver_app"]:
                 self.instance.deliver_app, _ = CommCareApp.objects.get_or_create(
                     cc_app_id=app["id"],
-                    name=app["name"],
                     cc_domain=app["domain"],
                     organization=organization,
                     defaults={
+                        "name": app["name"],
                         "created_by": self.user.email,
                         "modified_by": self.user.email,
                     },
