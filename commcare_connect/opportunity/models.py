a59ca098dc1b9b6e12a3e4d48131bf6571bc226a
Pawan Verma <pverma@dimagi.com>
2023-08-01 16:49:26 +0530

    Add learn progress models

--- commcare_connect/opportunity/models.py
+++ commcare_connect/opportunity/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from commcare_connect.users.models import Organization
+from commcare_connect.users.models import Organization, User
 from commcare_connect.utils.db import BaseModel
 
 
@@ -70,3 +70,70 @@ class DeliverForm(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class LearnModule(models.Model):
+    app = models.ForeignKey(
+        CommCareApp,
+        on_delete=models.CASCADE,
+        related_name="learn_module",
+        related_query_name="learn_module",
+    )
+    slug = models.SlugField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    time_estimate = models.IntegerField()
+
+
+class UserModule(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_modules",
+        related_query_name="user_module",
+    )
+    module = models.ForeignKey(
+        LearnModule,
+        on_delete=models.PROTECT,
+        related_name="user_modules",
+        related_query_name="user_module",
+    )
+    opportunity = models.ForeignKey(
+        Opportunity,
+        on_delete=models.PROTECT,
+        related_name="user_modules",
+        related_query_name="user_module",
+    )
+    date_completed = models.DateTimeField(blank=True, null=True)
+    duration = models.TimeField()
+    cc_form_id = models.CharField(max_length=25)
+    app_build_id = models.CharField(max_length=25)
+    app_build_version = models.IntegerField()
+
+
+class Assessment(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="assessments",
+        related_query_name="assessment",
+    )
+    app = models.ForeignKey(
+        CommCareApp,
+        on_delete=models.PROTECT,
+        related_name="assessments",
+        related_query_name="assessment",
+    )
+    opportunity = models.ForeignKey(
+        Opportunity,
+        on_delete=models.PROTECT,
+        related_name="assessments",
+        related_query_name="assessment",
+    )
+    date_accomplished = models.DateTimeField(blank=True, null=True)
+    score = models.IntegerField()
+    passing_score = models.IntegerField()
+    passed = models.BooleanField()
+    cc_form_id = models.CharField(max_length=25)
+    app_build_id = models.CharField(max_length=25)
+    app_build_version = models.IntegerField()
