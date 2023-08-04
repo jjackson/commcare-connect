75beeacd2b40d381fc50a9385e8e5a31ac7dd1e9
Simon Kelly <skelly@dimagi.com>
2023-08-04 15:41:56 +0200

    configure sentry

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,14 +1,8 @@
+import logging
 import os
 
 from .base import *  # noqa
 from .base import env
-
-# import sentry_sdk
-# from sentry_sdk.integrations.celery import CeleryIntegration
-# from sentry_sdk.integrations.django import DjangoIntegration
-# from sentry_sdk.integrations.logging import LoggingIntegration
-# from sentry_sdk.integrations.redis import RedisIntegration
-
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -76,27 +70,32 @@ INSTALLED_APPS += ["anymail"]  # noqa: F405
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
 ANYMAIL = {}
 
-# # Sentry
-# # ------------------------------------------------------------------------------
-# SENTRY_DSN = env("SENTRY_DSN")
-# SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
-#
-# sentry_logging = LoggingIntegration(
-#     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
-#     event_level=logging.ERROR,  # Send errors as events
-# )
-# integrations = [
-#     sentry_logging,
-#     DjangoIntegration(),
-#     CeleryIntegration(),
-#     RedisIntegration(),
-# ]
-# sentry_sdk.init(
-#     dsn=SENTRY_DSN,
-#     integrations=integrations,
-#     environment=env("SENTRY_ENVIRONMENT", default="production"),
-#     traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=0.0),
-# )
+# Sentry
+# ------------------------------------------------------------------------------
+SENTRY_DSN = env("SENTRY_DSN", default=None)
+if SENTRY_DSN:
+    import sentry_sdk
+    from sentry_sdk.integrations.celery import CeleryIntegration
+    from sentry_sdk.integrations.django import DjangoIntegration
+    from sentry_sdk.integrations.logging import LoggingIntegration
+    from sentry_sdk.integrations.redis import RedisIntegration
+
+    sentry_logging = LoggingIntegration(
+        level=logging.INFO,  # Capture info and above as breadcrumbs
+        event_level=logging.ERROR,  # Send errors as events
+    )
+    integrations = [
+        sentry_logging,
+        DjangoIntegration(),
+        CeleryIntegration(),
+        RedisIntegration(),
+    ]
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=integrations,
+        environment=env("SENTRY_ENVIRONMENT", default="production"),
+        traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=0.0),
+    )
 
 # django-rest-framework
 # -------------------------------------------------------------------------------
