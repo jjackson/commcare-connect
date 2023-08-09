c6372f3457a8677157fccfc581b15df8c8c9adc8
Pawan Verma <pverma@dimagi.com>
2023-08-09 15:54:00 +0530

    Add views and urls for learn progress and user learn progress

--- commcare_connect/opportunity/urls.py
+++ commcare_connect/opportunity/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
 
-from commcare_connect.opportunity.views import OpportunityCreate, OpportunityEdit, OpportunityList
+from commcare_connect.opportunity.views import (
+    OpportunityCreate,
+    OpportunityEdit,
+    OpportunityLearnProgressList,
+    OpportunityList,
+    OpportunityUserLearnProgress,
+)
 
 app_name = "opportunity"
 urlpatterns = [
     path("", view=OpportunityList.as_view(), name="list"),
     path("create/", view=OpportunityCreate.as_view(), name="create"),
     path("<int:pk>/", view=OpportunityEdit.as_view(), name="edit"),
+    path("<int:pk>/learn_progress/", view=OpportunityLearnProgressList.as_view(), name="learn_progress"),
+    path(
+        "<int:opp_id>/learn_progress/<int:pk>",
+        view=OpportunityUserLearnProgress.as_view(),
+        name="user_learn_progress",
+    ),
 ]
