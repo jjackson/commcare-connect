b8359a2e0997924daae6b1179dc4cb46b3ead928
Cal Ellowitz <calellowitz@gmail.com>
2023-08-11 11:08:38 -0400

    rename migration

--- commcare_connect/users/migrations/0007_user_username.py
+++ commcare_connect/users/migrations/0007_user_username.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("users", "0004_organization_members"),
+        ("users", "0006_alter_organization_options_and_more"),
     ]
 
     operations = [
