2c78b64d18e48d54c726a22fb42ddab95feba749
Cal Ellowitz <calellowitz@gmail.com>
2023-08-11 11:04:50 -0400

    lint

--- commcare_connect/users/models.py
+++ commcare_connect/users/models.py
@@ -18,7 +18,6 @@ class User(AbstractUser):
 
     username_validator = UnicodeUsernameValidator()
 
-
     # First and last name do not cover name patterns around the globe
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
