522512f762913d15be51cd851d478734ad771420
Cal Ellowitz <calellowitz@gmail.com>
2023-08-10 21:10:33 -0400

    add back username

--- commcare_connect/users/models.py
+++ commcare_connect/users/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -15,14 +16,29 @@ class User(AbstractUser):
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
+    username_validator = UnicodeUsernameValidator()
+
+
     # First and last name do not cover name patterns around the globe
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
     email = models.EmailField(_("email address"), unique=True)
-    username = None  # type: ignore
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+        null=True
+    )
 
-    USERNAME_FIELD = "email"
+
     REQUIRED_FIELDS = []
 
     objects = UserManager()
