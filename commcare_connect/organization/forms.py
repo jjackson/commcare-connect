db547dca5b10fc64fc871e0524f571192227f242
Simon Kelly <skelly@dimagi.com>
2023-08-15 15:48:44 +0200

    initial org home

---
+++ commcare_connect/organization/forms.py
@@ -0,0 +1,23 @@
+from crispy_forms import helper, layout
+from django import forms
+from django.utils.translation import gettext
+
+from commcare_connect.organization.models import Organization
+
+
+class OrganizationChangeForm(forms.ModelForm):
+    class Meta:
+        model = Organization
+        fields = ("name",)
+        labels = {
+            "name": gettext("Organization Name"),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = helper.FormHelper(self)
+        self.helper.layout = layout.Layout(
+            layout.Row(layout.Field("name")),
+            layout.Submit("submit", "Submit"),
+        )
