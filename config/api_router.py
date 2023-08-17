7461046c3cac2dfbbcabe0ca2a6127053a54942c
Pawan Verma <pverma@dimagi.com>
2023-08-17 18:30:50 +0530

    Add endpoint to fetch learn progress data for a user

--- config/api_router.py
+++ config/api_router.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from commcare_connect.form_receiver.views import FormReceiver
-from commcare_connect.opportunity.api.views import OpportunityViewSet
+from commcare_connect.opportunity.api.views import OpportunityViewSet, UserLearnProgressView
 from commcare_connect.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -18,4 +18,5 @@ app_name = "api"
 urlpatterns = [
     path("", include(router.urls)),
     path("receiver/", FormReceiver.as_view(), name="receiver"),
+    path("opportunity/<int:pk>/learn_progress", UserLearnProgressView.as_view()),
 ]
