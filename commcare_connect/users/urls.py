bf9969ef950c65e5841a3af6af0754c6bfdd2e42
Cal Ellowitz <calellowitz@gmail.com>
2023-08-18 18:18:34 -0400

    add create hq user view

--- commcare_connect/users/urls.py
+++ commcare_connect/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from commcare_connect.users.views import (
+    create_hq_user,
     create_user_link_view,
     user_detail_view,
     user_redirect_view,
@@ -13,4 +14,5 @@ urlpatterns = [
     path("update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
     path("create_user_link/", view=create_user_link_view, name="create_user_link"),
+    path("create_hq_user/", view=create_hq_user, name="create_hq_user"),
 ]
