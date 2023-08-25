f1bc223ad5779d6ae4ef39459742d82891308b01
Cal Ellowitz <calellowitz@gmail.com>
2023-08-24 22:06:09 -0400

    change view to start learn

--- commcare_connect/users/helpers.py
+++ commcare_connect/users/helpers.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from commcare_connect.organization.models import Organization
 
 
@@ -14,3 +16,17 @@ def get_organization_for_request(request, view_kwargs):
 
     membership = request.user.memberships.select_related("organization").first()
     return membership.organization if membership else None
+
+
+def create_hq_user(user, domain, app, api_key):
+    mobile_worker_api_url = f"{settings.COMMCARE_HQ_URL}/a/{domain}/api/v0.5/user"
+    hq_request  = requests.post(mobile_worker_api_url,
+                                data={
+                                    "username": user.username,
+                                    "connect_user": user.username,
+                                },
+                                headers={"Authorization": f"ApiKey {api_key.user.email}:{api_key.api_key}"}
+                                )
+    if hq_request.status_code == 201:
+        return True
+    return False
