67a507414e221b9d80289e2bf0055a203e214f94
Simon Kelly <skelly@dimagi.com>
2023-08-25 10:54:55 +0200

    generate dataset manually

--- commcare_connect/opportunity/export.py
+++ commcare_connect/opportunity/export.py
@@ -1,16 +1,21 @@
-from django_tables2.export import TableExport
+from django.utils.encoding import force_str
+from tablib import Dataset
 
 from commcare_connect.opportunity.models import Opportunity, UserVisit
 from commcare_connect.opportunity.tables import UserVisitTable
 
 
-def export_user_visits(opportunity: Opportunity, export_format: str) -> TableExport:
-    """
-    Export user visits for a given opportunity to a CSV file.
-    """
-    if not TableExport.is_valid_format(export_format):
-        raise ValueError(f"Invalid export format: {export_format}")
+def export_user_visits(opportunity: Opportunity) -> Dataset:
     user_visits = UserVisit.objects.filter(opportunity=opportunity)
     table = UserVisitTable(user_visits)
-    exporter = TableExport(export_format, table, exclude_columns=("visit_date",))
-    return exporter
+    exclude_columns = ("visit_date",)
+    columns = [
+        column
+        for column in table.columns.iterall()
+        if not (column.column.exclude_from_export or column.name in exclude_columns)
+    ]
+
+    dataset = Dataset(title="Export", headers=[force_str(column.header, strings_only=True) for column in columns])
+    for row in table.rows:
+        dataset.append([force_str(row.get_cell_value(column.name), strings_only=True) for column in columns])
+    return dataset
