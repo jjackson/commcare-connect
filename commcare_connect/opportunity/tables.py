c876d5833e6a31379614d7f3a9c306cada15ae96
Simon Kelly <skelly@dimagi.com>
2023-08-25 16:13:52 +0200

    update visit status via import

--- commcare_connect/opportunity/tables.py
+++ commcare_connect/opportunity/tables.py
@@ -22,17 +22,18 @@ class OpportunityAccessTable(tables.Table):
 
 class UserVisitTable(tables.Table):
     # export only columns
-    username = columns.Column(verbose_name="Username", accessor="user.username", visible=False)
-    form_json = columns.Column(verbose_name="Form JSON", accessor="form_json", visible=False)
+    visit_id = columns.Column("Visit ID", accessor="xform_id", visible=False)
+    username = columns.Column("Username", accessor="user.username", visible=False)
+    form_json = columns.Column("Form JSON", accessor="form_json", visible=False)
     visit_date_export = columns.DateTimeColumn(
         verbose_name="Visit date", accessor="visit_date", format="c", visible=False
     )
 
-    deliver_form = columns.Column(verbose_name="Form Name", accessor="deliver_form.name")
+    deliver_form = columns.Column("Form Name", accessor="deliver_form.name")
 
     class Meta:
         model = UserVisit
         fields = ("user.name", "visit_date", "status")
-        sequence = ("visit_date", "visit_date_export", "username", "user.name", "deliver_form", "status")
+        sequence = ("visit_id", "visit_date", "visit_date_export", "status", "username", "user.name", "deliver_form")
         empty_text = "No forms."
         orderable = False
--- commcare_connect/opportunity/urls.py
+++ commcare_connect/opportunity/urls.py
@@ -9,6 +9,7 @@ from commcare_connect.opportunity.views import (
     OpportunityUserTableView,
     OpportunityUserVisitTableView,
     export_user_visits,
+    update_visit_status_import,
 )
 
 app_name = "opportunity"
@@ -20,6 +21,7 @@ urlpatterns = [
     path("<int:pk>/user_table/", view=OpportunityUserTableView.as_view(), name="user_table"),
     path("<int:pk>/visit_table/", view=OpportunityUserVisitTableView.as_view(), name="visit_table"),
     path("<int:pk>/visit_export/", view=export_user_visits, name="visit_export"),
+    path("<int:pk>/visit_import/", view=update_visit_status_import, name="visit_import"),
     path(
         "<int:opp_id>/learn_progress/<int:pk>",
         view=OpportunityUserLearnProgress.as_view(),
