8311d20315867ff4e36f7a29e4c84f366260a019
Cal Ellowitz <calellowitz@gmail.com>
2023-08-28 16:26:57 -0400

    more migration conflicts

--- commcare_connect/opportunity/migrations/0014_opportunityaccess_date_learn_started.py
+++ commcare_connect/opportunity/migrations/0014_opportunityaccess_date_learn_started.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("opportunity", "0012_uservisit_opportunity_status"),
+        ("opportunity", "0013_hqapikey_opportunity_api_key"),
     ]
 
     operations = [
