d71b00e202679c5cec68f8d14f68c39aedc9fe87
Cal Ellowitz <calellowitz@gmail.com>
2023-08-28 16:26:57 -0400

    more migration conflicts

--- commcare_connect/opportunity/migrations/0013_hqapikey_opportunity_api_key.py
+++ commcare_connect/opportunity/migrations/0013_hqapikey_opportunity_api_key.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("opportunity", "0010_move_org"),
+        ("opportunity", "0012_uservisit_opportunity_status"),
     ]
 
     operations = [
