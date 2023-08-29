3160648cedd30598ddb49b9c98060ee5c84979da
Simon Kelly <skelly@dimagi.com>
2023-08-29 17:42:18 +0200

    fix opportunity access create

--- commcare_connect/opportunity/tasks.py
+++ commcare_connect/opportunity/tasks.py
@@ -35,4 +35,4 @@ def add_connect_users(user_list, opportunity):
     data = result.json()
     for user in data["found_users"]:
         u = User.objects.get_or_create(username=user["username"])
-        OpportunityAccess.get_or_create(user=u, opportunity=opportunity)
+        OpportunityAccess.objects.get_or_create(user=u, opportunity=opportunity)
