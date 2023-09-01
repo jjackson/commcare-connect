ae224b7718395197d87aada97db2252323610ae1
Simon Kelly <skelly@dimagi.com>
2023-09-01 13:05:52 +0200

    deploy with kamal

--- deploy/vault_password.sh
+++ deploy/vault_password.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script is used by Ansible to retrieve the Ansible Vault password from 1Password.
+# Use it by passing `--vault-password-file=vault_password.sh` to Ansible.
+
 VAULT_ID="CommCare Connect"
 VAULT_ANSIBLE_NAME="Ansible Vault"
 op item get --vault="$VAULT_ID" "$VAULT_ANSIBLE_NAME" --fields password
