56dbac61746f804d715426852e805d3e6c6cff4c
Cal Ellowitz <calellowitz@gmail.com>
2023-09-19 20:00:15 -0400

    lint

--- commcare_connect/users/managers.py
+++ commcare_connect/users/managers.py
@@ -18,12 +18,16 @@ class UserManager(DjangoUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, username: str | None = None, email: str | None = None, password: str | None = None, **extra_fields):
+    def create_user(
+        self, username: str | None = None, email: str | None = None, password: str | None = None, **extra_fields
+    ):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(username, email, password, **extra_fields)
 
-    def create_superuser(self, username: str | None = None, email: str | None = None, password: str | None = None, **extra_fields):
+    def create_superuser(
+        self, username: str | None = None, email: str | None = None, password: str | None = None, **extra_fields
+    ):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
