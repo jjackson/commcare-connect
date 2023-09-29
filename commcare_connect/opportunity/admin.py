562fadb54bfe5fd44aae248e2ce12dbc9fc3417a
Simon Kelly <skelly@dimagi.com>
2023-09-29 09:52:43 +0200

    swap deliver form for deliver unit in admin

--- commcare_connect/opportunity/admin.py
+++ commcare_connect/opportunity/admin.py
@@ -5,7 +5,7 @@ from commcare_connect.opportunity.models import (
     Assessment,
     CommCareApp,
     CompletedModule,
-    DeliverForm,
+    DeliverUnit,
     LearnModule,
     Opportunity,
     OpportunityAccess,
@@ -18,7 +18,7 @@ from commcare_connect.opportunity.models import (
 
 admin.site.register(Opportunity)
 admin.site.register(CommCareApp)
-admin.site.register(DeliverForm)
+admin.site.register(DeliverUnit)
 admin.site.register(LearnModule)
 admin.site.register(CompletedModule)
 admin.site.register(Assessment)
