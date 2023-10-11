224656991af8ba18b97e9da2c99c95cc5f4ea61e
Pawan Verma <pverma@dimagi.com>
2023-10-11 19:30:38 +0530

    Add tests for learn progress api

--- commcare_connect/opportunity/tests/test_api_views.py
+++ commcare_connect/opportunity/tests/test_api_views.py
@@ -1,9 +1,14 @@
 import datetime
 
+import pytest
 from rest_framework.test import APIClient
 
-from commcare_connect.opportunity.models import OpportunityClaim
-from commcare_connect.opportunity.tests.factories import OpportunityAccessFactory, OpportunityFactory
+from commcare_connect.opportunity.models import Assessment, CompletedModule, OpportunityClaim
+from commcare_connect.opportunity.tests.factories import (
+    LearnModuleFactory,
+    OpportunityAccessFactory,
+    OpportunityFactory,
+)
 from commcare_connect.users.models import User
 from commcare_connect.users.tests.factories import ConnectIdUserLinkFactory
 
@@ -61,3 +66,36 @@ def test_claim_endpoint_already_claimed_opportunity(mobile_user: User, api_clien
     response = api_client.post(f"/api/opportunity/{opportunity.id}/claim")
     assert response.status_code == 200
     assert response.data == "Opportunity is already claimed"
+
+
+@pytest.mark.django_db
+def test_learn_progress_endpoint(mobile_user: User, api_client: APIClient):
+    opportunity, opportunity_access = _setup_opportunity_and_access(
+        mobile_user, total_budget=1000, end_date=datetime.date.today() + datetime.timedelta(days=100)
+    )
+    learn_module = LearnModuleFactory(slug="module_1", app=opportunity.learn_app)
+    CompletedModule.objects.create(
+        module=learn_module,
+        user=mobile_user,
+        opportunity=opportunity,
+        date=datetime.datetime.now(),
+        duration=datetime.timedelta(hours=10),
+    )
+    Assessment.objects.create(
+        user=mobile_user,
+        app=opportunity.learn_app,
+        opportunity=opportunity,
+        date=datetime.datetime.now(),
+        score=100,
+        passing_score=opportunity.learn_app.passing_score,
+        passed=True,
+    )
+    api_client.force_authenticate(mobile_user)
+    response = api_client.get(f"/api/opportunity/{opportunity.id}/learn_progress")
+    assert response.status_code == 200
+    assert "completed_modules" in response.data
+    assert len(response.data["completed_modules"]) == 1
+    assert "assessments" in response.data
+    assert len(response.data["assessments"]) == 1
+    assert list(response.data["completed_modules"][0].keys()) == ["module", "date", "duration"]
+    assert list(response.data["assessments"][0].keys()) == ["date", "score", "passing_score", "passed"]
