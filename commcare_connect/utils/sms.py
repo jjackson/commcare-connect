fb99ad3753d1e4987a45f91af4d2560ac53fef2a
Cal Ellowitz <calellowitz@gmail.com>
2023-10-15 18:52:22 -0400

    first pass at alphanumeric sender

--- commcare_connect/utils/sms.py
+++ commcare_connect/utils/sms.py
@@ -10,4 +10,13 @@ def send_sms(to, body):
     if not (settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN and settings.TWILIO_MESSAGING_SERVICE):
         raise SMSException("Twilio credentials not provided")
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    client.messages.create(body=body, to=to, messaging_service_sid=settings.TWILIO_MESSAGING_SERVICE)
+    sender = get_sms_sender(to)
+    client.messages.create(body=body, to=to, from_=sender, messaging_service_sid=settings.TWILIO_MESSAGING_SERVICE)
+
+
+def get_sms_sender(number):
+    SMS_SENDERS = {"+265": "ConnectID"}
+    for code, sender in SMS_SENDERS.items():
+        if number.startswith(code):
+            return sender
+    return None
