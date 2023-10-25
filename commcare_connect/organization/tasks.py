938050f28f9c5832f2d66d0778f16c926ba8a6df
Pawan Verma <pverma@dimagi.com>
2023-10-25 12:24:51 +0530

    Fix email indent

--- commcare_connect/organization/tasks.py
+++ commcare_connect/organization/tasks.py
@@ -18,13 +18,13 @@ def send_org_invite(membership_id, host_user_id):
     invite_url = build_absolute_uri(None, location)
     message = f"""Hi,
 
-    You have been invited to join {membership.organization.name} on Commcare Connect by {host_user.name}.
-    The invite can be accepted by visiting the link.
+You have been invited to join {membership.organization.name} on Commcare Connect by {host_user.name}.
+The invite can be accepted by visiting the link.
 
-    {invite_url}
+{invite_url}
 
-    Thank You,
-    Commcare Connect"""
+Thank You,
+Commcare Connect"""
     send_mail(
         subject=f"{host_user.name} has invite you to join '{membership.organization.name}' on CommCare Connect",
         message=message,
