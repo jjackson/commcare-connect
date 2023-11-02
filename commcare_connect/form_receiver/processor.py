f10d4b9b84c0287fac52c3c04a7fd3612cd335a3
Pawan Verma <pverma@dimagi.com>
2023-11-02 12:21:28 +0530

    Fix max visits counting the over_limit visits

--- commcare_connect/form_receiver/processor.py
+++ commcare_connect/form_receiver/processor.py
@@ -138,12 +138,15 @@ def process_deliver_form(user, xform: XForm, app: CommCareApp, opportunity: Oppo
 
 def process_deliver_unit(user, xform: XForm, app: CommCareApp, opportunity: Opportunity, deliver_unit_block: dict):
     deliver_unit = get_or_create_deliver_unit(app, deliver_unit_block)
-    counts = UserVisit.objects.filter(opportunity=opportunity, user=user).aggregate(
-        daily=Count("pk", filter=Q(visit_date__date=xform.metadata.timeStart)), total=Count("*")
+    counts = (
+        UserVisit.objects.filter(opportunity=opportunity, user=user)
+        .exclude(status=VisitValidationStatus.over_limit)
+        .aggregate(
+            daily=Count("pk", filter=Q(visit_date__date=xform.metadata.timeStart)),
+            total=Count("*"),
+        )
     )
-    claim = OpportunityClaim.objects.filter(
-        opportunity_access__opportunity=opportunity, opportunity_access__user=user
-    ).first()
+    claim = OpportunityClaim.objects.get(opportunity_access__opportunity=opportunity, opportunity_access__user=user)
     user_visit = UserVisit(
         opportunity=opportunity,
         user=user,
