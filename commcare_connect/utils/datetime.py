80292936a77a24763cae5f233adedae88605635f
Pawan Verma <pverma@dimagi.com>
2023-11-07 13:25:25 +0530

    Add check date based on equal

--- commcare_connect/utils/datetime.py
+++ commcare_connect/utils/datetime.py
@@ -3,5 +3,6 @@ import datetime
 from django.utils.timezone import now
 
 
-def is_date_before(date, days: int):
-    return date <= now() - datetime.timedelta(days=days)
+def is_date_before(date: datetime.datetime, days: int):
+    before_date = now() - datetime.timedelta(days=days)
+    return date.date() == before_date.date()
