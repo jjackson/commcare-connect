664cae4f2764fe1e452a572ec580c694e6f01ef1
Cal Ellowitz <calellowitz@gmail.com>
2023-11-16 16:18:03 -0500

    return 400 for bad claim and update max_payment logic

--- commcare_connect/opportunity/tests/test_api_views.py
+++ commcare_connect/opportunity/tests/test_api_views.py
@@ -13,8 +13,14 @@ from commcare_connect.users.models import User
 from commcare_connect.users.tests.factories import ConnectIdUserLinkFactory
 
 
-def _setup_opportunity_and_access(mobile_user: User, total_budget, end_date):
-    opportunity = OpportunityFactory(total_budget=total_budget, max_visits_per_user=100, end_date=end_date)
+def _setup_opportunity_and_access(mobile_user: User, total_budget, end_date, claimed_budget=0, budget_per_visit=10):
+    opportunity = OpportunityFactory(
+        total_budget=total_budget,
+        claimed_budget=claimed_budget,
+        max_visits_per_user=100,
+        end_date=end_date,
+        budget_per_visit=budget_per_visit,
+    )
     opportunity_access = OpportunityAccessFactory(opportunity=opportunity, user=mobile_user)
     ConnectIdUserLinkFactory(
         user=mobile_user, commcare_username="test@ccc-test.commcarehq.org", domain=opportunity.deliver_app.cc_domain
@@ -39,7 +45,7 @@ def test_claim_endpoint_budget_exhausted(mobile_user: User, api_client: APIClien
     )
     api_client.force_authenticate(mobile_user)
     response = api_client.post(f"/api/opportunity/{opportunity.id}/claim")
-    assert response.status_code == 200
+    assert response.status_code == 400
     assert response.data == "Opportunity cannot be claimed. (Budget Exhausted)"
 
 
@@ -49,7 +55,7 @@ def test_claim_endpoint_end_date_exceeded(mobile_user: User, api_client: APIClie
     )
     api_client.force_authenticate(mobile_user)
     response = api_client.post(f"/api/opportunity/{opportunity.id}/claim")
-    assert response.status_code == 200
+    assert response.status_code == 400
     assert response.data == "Opportunity cannot be claimed. (End date reached)"
 
 
@@ -66,6 +72,36 @@ def test_claim_endpoint_already_claimed_opportunity(mobile_user: User, api_clien
     response = api_client.post(f"/api/opportunity/{opportunity.id}/claim")
     assert response.status_code == 200
     assert response.data == "Opportunity is already claimed"
+
+
+def test_claim_endpoint_less_budget_than_visit(mobile_user: User, api_client: APIClient):
+    opportunity, opportunity_access = _setup_opportunity_and_access(
+        mobile_user,
+        total_budget=10,
+        end_date=datetime.date.today() + datetime.timedelta(days=100),
+        claimed_budget=9,
+        budget_per_visit=2,
+    )
+    api_client.force_authenticate(mobile_user)
+    response = api_client.post(f"/api/opportunity/{opportunity.id}/claim")
+    assert response.status_code == 400
+    assert response.data == "Opportunity cannot be claimed. (Budget Exhausted)"
+
+
+def test_claim_endpoint_uneven_visits(mobile_user: User, api_client: APIClient):
+    opportunity, opportunity_access = _setup_opportunity_and_access(
+        mobile_user,
+        total_budget=10,
+        end_date=datetime.date.today() + datetime.timedelta(days=100),
+        claimed_budget=7,
+        budget_per_visit=2,
+    )
+    api_client.force_authenticate(mobile_user)
+    response = api_client.post(f"/api/opportunity/{opportunity.id}/claim")
+    assert response.status_code == 201
+    claim = OpportunityClaim.objects.filter(opportunity_access=opportunity_access)
+    assert claim.exists()
+    assert claim.max_payments == 1
 
 
 @pytest.mark.django_db
