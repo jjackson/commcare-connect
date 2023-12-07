f3321fb127daacb4377fb355070788ccb959c9a2
Pawan Verma <pverma@dimagi.com>
2023-12-07 13:09:53 +0530

    Add job_claimed annotation and aggregate column

--- commcare_connect/opportunity/helpers.py
+++ commcare_connect/opportunity/helpers.py
@@ -22,6 +22,7 @@ def get_annotated_opportunity_access(opportunity: Opportunity):
             completed_modules_count=Count(
                 "user__completed_modules", filter=Q(user__completed_modules__opportunity=opportunity)
             ),
+            job_claimed=Case(When(Q(opportunityclaim__isnull=False), then="opportunityclaim__date_claimed")),
         )
         .annotate(
             date_learn_completed=Case(
