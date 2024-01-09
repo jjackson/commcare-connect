437f0274cac533ee94f915a29d6effbac3963fa0
Pawan Verma <pverma@dimagi.com>
2024-01-09 18:49:22 +0530

    Add url for payment_delete

--- commcare_connect/opportunity/urls.py
+++ commcare_connect/opportunity/urls.py
@@ -21,6 +21,7 @@ from commcare_connect.opportunity.views import (
     export_user_status,
     export_user_visits,
     export_users_for_payment,
+    payment_delete,
     payment_import,
     update_visit_status_import,
     user_visits_list,
@@ -59,4 +60,5 @@ urlpatterns = [
     path("<int:pk>/deliver_status_table/", view=OpportunityDeliverStatusTable.as_view(), name="deliver_status_table"),
     path("<int:pk>/deliver_status_export/", view=export_deliver_status, name="deliver_status_export"),
     path("<int:opp_id>/user_visits/<int:pk>/", view=user_visits_list, name="user_visits_list"),
+    path("<int:opp_id>/payment/<int:access_id>/delete/<int:pk>/", view=payment_delete, name="payment_delete"),
 ]
