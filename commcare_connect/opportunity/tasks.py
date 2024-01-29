a8e5ed99f3a604451da438a1ec9c8a768358fec1
Pawan Verma <pverma@dimagi.com>
2024-01-29 13:03:01 +0530

    Refactor opportunity tasks

--- commcare_connect/opportunity/tasks.py
+++ commcare_connect/opportunity/tasks.py
@@ -19,14 +19,12 @@ from commcare_connect.opportunity.export import (
 )
 from commcare_connect.opportunity.forms import DateRanges
 from commcare_connect.opportunity.models import (
-    CompletedModule,
     DeliverUnit,
     LearnModule,
     Opportunity,
     OpportunityAccess,
     OpportunityClaim,
     Payment,
-    UserVisit,
     VisitValidationStatus,
 )
 from commcare_connect.users.models import User
@@ -165,9 +163,7 @@ def _get_inactive_message(access: OpportunityAccess):
 
 
 def _get_learn_message(access: OpportunityAccess):
-    last_user_learn_module = (
-        CompletedModule.objects.filter(user=access.user, opportunity=access.opportunity).order_by("date").last()
-    )
+    last_user_learn_module = access.completedmodule_set.order_by("date").last()
     if last_user_learn_module and is_date_before(last_user_learn_module.date, days=3):
         return Message(
             usernames=[access.user.username],
@@ -180,9 +176,7 @@ def _get_learn_message(access: OpportunityAccess):
 
 
 def _check_deliver_inactive(access: OpportunityAccess):
-    last_user_deliver_visit = (
-        UserVisit.objects.filter(user=access.user, opportunity=access.opportunity).order_by("visit_date").last()
-    )
+    last_user_deliver_visit = access.uservisit_set.order_by("visit_date").last()
     if last_user_deliver_visit and is_date_before(last_user_deliver_visit.visit_date, days=2):
         return _get_deliver_message(access)
 
