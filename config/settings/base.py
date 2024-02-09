decb044820f9bfab1a3bacc268a17c73ca711ef9
nemanja <nemanja.stancic.91@gmail.com>
2024-02-09 13:55:52 -0500

    set table style globally; replace allotted_visits_per_user property with max_visits_per_user;

--- config/settings/base.py
+++ config/settings/base.py
@@ -293,9 +293,9 @@ CACHES = {
 
 DJANGO_TABLES2_TEMPLATE = "tables/tabbed_table.html"
 DJANGO_TABLES2_TABLE_ATTRS = {
-    "class": "table border table-custom",
+    "class": "table table-bordered mb-0",
     "thead": {
-        "class": "table-secondary",
+        "class": "",
     },
     "tfoot": {
         "class": "table-light fw-bold",
