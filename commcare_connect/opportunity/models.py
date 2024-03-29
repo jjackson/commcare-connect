55f15c317700e15eac88ae2eb6dd0f5515e587f6
Pawan Verma <pverma@dimagi.com>
2024-03-29 18:42:52 +0530

    Refactor filter deliver units logic

--- commcare_connect/opportunity/models.py
+++ commcare_connect/opportunity/models.py
@@ -298,8 +298,11 @@ class CompletedWork(models.Model):
             "deliver_unit_id", flat=True
         )
         unit_counts = Counter(visits)
-        required_deliver_units = self.payment_unit.deliver_units.filter(optional=False).values_list("id", flat=True)
-        optional_deliver_units = self.payment_unit.deliver_units.filter(optional=True).values_list("id", flat=True)
+        deliver_units = self.payment_unit.deliver_units.values("id", "optional")
+        required_deliver_units = list(
+            du["id"] for du in filter(lambda du: not du.get("optional", False), deliver_units)
+        )
+        optional_deliver_units = list(du["id"] for du in filter(lambda du: du.get("optional", False), deliver_units))
         # NOTE: The min unit count is the completed required deliver units for an entity_id
         number_completed = min(unit_counts[deliver_id] for deliver_id in required_deliver_units)
         if optional_deliver_units:
