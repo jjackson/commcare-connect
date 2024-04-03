35eb56dcfac3c706c68809dcb227317139efb114
Pawan Verma <pverma@dimagi.com>
2024-04-03 12:54:37 +0530

    update tests

--- commcare_connect/opportunity/tests/test_helpers.py
+++ commcare_connect/opportunity/tests/test_helpers.py
@@ -1,8 +1,8 @@
 import pytest
 
 from commcare_connect.opportunity.helpers import get_annotated_opportunity_access_deliver_status
-from commcare_connect.opportunity.models import Opportunity, UserVisit
-from commcare_connect.opportunity.tests.factories import OpportunityAccessFactory, UserVisitFactory
+from commcare_connect.opportunity.models import Opportunity
+from commcare_connect.opportunity.tests.factories import CompletedWorkFactory, OpportunityAccessFactory
 from commcare_connect.users.tests.factories import MobileUserFactory
 
 
@@ -27,26 +27,24 @@ def test_deliver_status_query_no_visits(opportunity: Opportunity):
 @pytest.mark.django_db
 def test_deliver_status_query(opportunity: Opportunity):
     mobile_users = MobileUserFactory.create_batch(5)
-    user_visit_counts = {}
+    completed_work_counts = {}
     for mobile_user in mobile_users:
-        OpportunityAccessFactory(opportunity=opportunity, user=mobile_user, accepted=True)
-        user_visits = UserVisitFactory.create_batch(20, opportunity=opportunity, user=mobile_user)
-        count_by_status = dict(approved=0, pending=0, rejected=0, over_limit=0, completed=0, duplicate=0)
-        for user_visit in user_visits:
+        access = OpportunityAccessFactory(opportunity=opportunity, user=mobile_user, accepted=True)
+        completed_works = CompletedWorkFactory.create_batch(20, opportunity_access=access)
+        count_by_status = dict(approved=0, pending=0, rejected=0, completed=0)
+        for user_visit in completed_works:
             count_by_status[user_visit.status.value] += 1
-        count_by_status["completed"] = len(user_visits)
-        user_visit_counts[mobile_user.username] = count_by_status
+        count_by_status["completed"] = len(completed_works)
+        completed_work_counts[mobile_user.username] = count_by_status
 
     access_objects = get_annotated_opportunity_access_deliver_status(opportunity)
     for access in access_objects:
         username = access.user.username
-        assert username in user_visit_counts
-        assert user_visit_counts[username]["approved"] == access.visits_approved
-        assert user_visit_counts[username]["rejected"] == access.visits_rejected
-        assert user_visit_counts[username]["pending"] == access.visits_pending
-        assert user_visit_counts[username]["over_limit"] == access.visits_over_limit
-        assert user_visit_counts[username]["completed"] == access.visits_completed
-        assert user_visit_counts[username]["duplicate"] == access.visits_duplicate
+        assert username in completed_work_counts
+        assert completed_work_counts[username]["approved"] == access.approved
+        assert completed_work_counts[username]["rejected"] == access.rejected
+        assert completed_work_counts[username]["pending"] == access.pending
+        assert completed_work_counts[username]["completed"] == access.completed
 
 
 @pytest.mark.django_db
@@ -56,15 +54,12 @@ def test_deliver_status_query_visits_another_opportunity(opportunity: Opportunit
     mobile_users = list(MobileUserFactory.create_batch(5))
     for mobile_user in mobile_users:
         OpportunityAccessFactory(opportunity=opportunity, user=mobile_user, accepted=True)
-        UserVisitFactory.create_batch(5, user=mobile_user)
+        CompletedWorkFactory.create_batch(5)
     access_objects = get_annotated_opportunity_access_deliver_status(opportunity)
     usernames = {user.username for user in mobile_users}
     for access in access_objects:
-        assert UserVisit.objects.filter(user=mobile_user).count() == 5
         assert access.user.username in usernames
         assert access.visits_approved == 0
         assert access.visits_rejected == 0
         assert access.visits_pending == 0
-        assert access.visits_over_limit == 0
         assert access.visits_completed == 0
-        assert access.visits_duplicate == 0
