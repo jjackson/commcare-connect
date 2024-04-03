cea7fc85740d5d2c0ed8ab94d54c239b31ea0cc6
Pawan Verma <pverma@dimagi.com>
2024-04-03 15:19:47 +0530

    Add auto reject visits if flagged

--- commcare_connect/form_receiver/processor.py
+++ commcare_connect/form_receiver/processor.py
@@ -210,12 +210,8 @@ def process_deliver_unit(user, xform: XForm, app: CommCareApp, opportunity: Oppo
         user_visit.flagged = True
         user_visit.flag_reason = {"flags": flags}
 
-    if (
-        opportunity.auto_approve_visits
-        and not user_visit.flagged
-        and user_visit.status == VisitValidationStatus.pending
-    ):
-        user_visit.status = VisitValidationStatus.approved
+    if opportunity.auto_approve_visits and user_visit.status == VisitValidationStatus.pending:
+        user_visit.status = VisitValidationStatus.rejected if user_visit.flagged else VisitValidationStatus.approved
     user_visit.save()
     if opportunity.auto_approve_payments:
         approve_completed_work_and_update_payment_accrued.delay([completed_work.id])
