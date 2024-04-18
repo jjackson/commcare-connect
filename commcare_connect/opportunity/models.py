7c537f027574aa465f79feaa529ee803b0ef0d6b
Pawan Verma <pverma@dimagi.com>
2024-04-18 12:39:58 +0530

    Add additional choices to invite status

--- commcare_connect/opportunity/models.py
+++ commcare_connect/opportunity/models.py
@@ -494,13 +494,14 @@ class BlobMeta(models.Model):
 class UserInviteStatus(models.TextChoices):
     sms_delivered = "sms_delivered", gettext("SMS Delivered")
     sms_not_delivered = "sms_not_delivered", gettext("SMS Not Delivered")
-    sms_sent = "sms_sent", gettext("SMS Sent")
     accepted = "accepted", gettext("Accepted")
+    invited = "invited", gettext("Invited")
+    not_found = "not_found", gettext("ConnectID Not Found")
 
 
 class UserInvite(models.Model):
     opportunity = models.ForeignKey(Opportunity, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
     opportunity_access = models.OneToOneField(OpportunityAccess, on_delete=models.CASCADE, null=True, blank=True)
-    message_sid = models.CharField(max_length=25)
-    status = models.CharField(choices=UserInviteStatus.choices, default=UserInviteStatus.sms_sent)
+    message_sid = models.CharField(max_length=25, null=True, blank=True)
+    status = models.CharField(max_length=50, choices=UserInviteStatus.choices, default=UserInviteStatus.invited)
