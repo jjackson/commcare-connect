07d0be9dc243ba6e01fb3f69b1b4c58c6793ac86
Pawan Verma <pverma@dimagi.com>
2024-04-19 17:02:04 +0530

    Fix tests to include two payment units

--- commcare_connect/opportunity/tests/test_helpers.py
+++ commcare_connect/opportunity/tests/test_helpers.py
@@ -2,7 +2,11 @@ import pytest
 
 from commcare_connect.opportunity.helpers import get_annotated_opportunity_access_deliver_status
 from commcare_connect.opportunity.models import Opportunity
-from commcare_connect.opportunity.tests.factories import CompletedWorkFactory, OpportunityAccessFactory
+from commcare_connect.opportunity.tests.factories import (
+    CompletedWorkFactory,
+    OpportunityAccessFactory,
+    PaymentUnitFactory,
+)
 from commcare_connect.users.tests.factories import MobileUserFactory
 
 
@@ -16,35 +20,34 @@ def test_deliver_status_query_no_visits(opportunity: Opportunity):
     usernames = {user.username for user in mobile_users}
     for access in access_objects:
         assert access.user.username in usernames
-        assert access.visits_approved == 0
-        assert access.visits_rejected == 0
-        assert access.visits_pending == 0
-        assert access.visits_over_limit == 0
-        assert access.visits_completed == 0
-        assert access.visits_duplicate == 0
+        assert access.approved == 0
+        assert access.rejected == 0
+        assert access.pending == 0
+        assert access.completed == 0
 
 
 @pytest.mark.django_db
 def test_deliver_status_query(opportunity: Opportunity):
     mobile_users = MobileUserFactory.create_batch(5)
     completed_work_counts = {}
+    payment_units = PaymentUnitFactory.create_batch(2, opportunity=opportunity)
     for mobile_user in mobile_users:
         access = OpportunityAccessFactory(opportunity=opportunity, user=mobile_user, accepted=True)
-        completed_works = CompletedWorkFactory.create_batch(20, opportunity_access=access)
-        count_by_status = dict(approved=0, pending=0, rejected=0, completed=0)
-        for user_visit in completed_works:
-            count_by_status[user_visit.status.value] += 1
-        count_by_status["completed"] = len(completed_works)
-        completed_work_counts[mobile_user.username] = count_by_status
-
+        for pu in payment_units:
+            count_by_status = dict(approved=0, pending=0, rejected=0, completed=0)
+            completed_works = CompletedWorkFactory.create_batch(20, opportunity_access=access, payment_unit=pu)
+            for cw in completed_works:
+                count_by_status[cw.status.value] += 1
+            count_by_status["completed"] = len(completed_works)
+            completed_work_counts[(mobile_user.username, pu.name)] = count_by_status
     access_objects = get_annotated_opportunity_access_deliver_status(opportunity)
     for access in access_objects:
         username = access.user.username
-        assert username in completed_work_counts
-        assert completed_work_counts[username]["approved"] == access.approved
-        assert completed_work_counts[username]["rejected"] == access.rejected
-        assert completed_work_counts[username]["pending"] == access.pending
-        assert completed_work_counts[username]["completed"] == access.completed
+        assert (username, access.payment_unit) in completed_work_counts
+        assert completed_work_counts[(username, access.payment_unit)]["approved"] == access.approved
+        assert completed_work_counts[(username, access.payment_unit)]["rejected"] == access.rejected
+        assert completed_work_counts[(username, access.payment_unit)]["pending"] == access.pending
+        assert completed_work_counts[(username, access.payment_unit)]["completed"] == access.completed
 
 
 @pytest.mark.django_db
@@ -59,7 +62,7 @@ def test_deliver_status_query_visits_another_opportunity(opportunity: Opportunit
     usernames = {user.username for user in mobile_users}
     for access in access_objects:
         assert access.user.username in usernames
-        assert access.visits_approved == 0
-        assert access.visits_rejected == 0
-        assert access.visits_pending == 0
-        assert access.visits_completed == 0
+        assert access.approved == 0
+        assert access.rejected == 0
+        assert access.pending == 0
+        assert access.completed == 0
