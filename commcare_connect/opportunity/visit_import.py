d0653a7807afc48d2da9e0e48209169ca8bfc99d
Pawan Verma <pverma@dimagi.com>
2024-05-16 16:33:28 +0530

    Add remove suspended users from payment import

--- commcare_connect/opportunity/visit_import.py
+++ commcare_connect/opportunity/visit_import.py
@@ -228,9 +228,9 @@ def _bulk_update_payments(opportunity: Opportunity, imported_data: Dataset) -> P
     payment_ids = []
     with transaction.atomic():
         usernames = list(payments)
-        users = OpportunityAccess.objects.filter(user__username__in=usernames, opportunity=opportunity).select_related(
-            "user"
-        )
+        users = OpportunityAccess.objects.filter(
+            user__username__in=usernames, opportunity=opportunity, suspended=False
+        ).select_related("user")
         for access in users:
             username = access.user.username
             amount = payments[username]
