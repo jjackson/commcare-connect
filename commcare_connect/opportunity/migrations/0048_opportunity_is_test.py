668e6a72fde244f67c50749088500e244470fb67
Cal Ellowitz <calellowitz@gmail.com>
2024-06-21 14:25:26 -0400

    update migration numbering

--- commcare_connect/opportunity/migrations/0048_opportunity_is_test.py
+++ commcare_connect/opportunity/migrations/0048_opportunity_is_test.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("opportunity", "0046_opportunityaccess_suspended_and_more"),
+        ("opportunity", "0047_alter_completedwork_status"),
     ]
 
     operations = [
