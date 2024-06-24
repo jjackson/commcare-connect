08dacc3d2ab1940cc0e9a4d61c9e2daa7efc0603
Pawan Verma <pverma@dimagi.com>
2024-06-24 19:02:50 +0530

    Refactor move user_invite out of the try block

--- commcare_connect/users/views.py
+++ commcare_connect/users/views.py
@@ -108,14 +108,14 @@ def start_learn_app(request):
         ConnectIDUserLink.objects.create(commcare_username=cc_username, user=request.user, domain=domain)
     try:
         access_object = OpportunityAccess.objects.get(user=request.user, opportunity=opportunity)
-        user_invite = UserInvite.objects.get(opportunity_access=access_object)
     except OpportunityAccess.DoesNotExist:
         return HttpResponse("user has no access to opportunity", status=400)
     with transaction.atomic():
         access_object.date_learn_started = now()
         access_object.accepted = True
-        user_invite.status = UserInviteStatus.accepted
         access_object.save()
+        user_invite = UserInvite.objects.get(opportunity_access=access_object)
+        user_invite.status = UserInviteStatus.accepted
         user_invite.save()
     return HttpResponse(status=200)
 
@@ -124,13 +124,13 @@ def start_learn_app(request):
 def accept_invite(request, invite_id):
     try:
         o = OpportunityAccess.objects.get(invite_id=invite_id)
-        user_invite = UserInvite.objects.get(opportunity_access=o)
     except OpportunityAccess.DoesNotExist:
         return HttpResponse("This link is invalid. Please try again", status=404)
     with transaction.atomic():
         o.accepted = True
-        user_invite.status = UserInviteStatus.accepted
         o.save()
+        user_invite = UserInvite.objects.get(opportunity_access=o)
+        user_invite.status = UserInviteStatus.accepted
         user_invite.save()
     return HttpResponse(
         "Thank you for accepting the invitation. Open your CommCare Connect App to "
