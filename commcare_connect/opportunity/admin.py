ecfe8b6587cd12bfd600e043c7c321119c936e05
Pawan Verma <pverma@dimagi.com>
2024-07-29 17:45:48 +0530

    Add opportunity claim limits to admin

--- commcare_connect/opportunity/admin.py
+++ commcare_connect/opportunity/admin.py
@@ -12,6 +12,7 @@ from commcare_connect.opportunity.models import (
     Opportunity,
     OpportunityAccess,
     OpportunityClaim,
+    OpportunityClaimLimit,
     Payment,
     PaymentUnit,
     UserInvite,
@@ -70,9 +71,15 @@ class LearnModuleAndDeliverUnitAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 
+class OpportunityClaimLimitInline(admin.TabularInline):
+    list_display = ["payment_unit", "max_visit"]
+    model = OpportunityClaimLimit
+
+
 @admin.register(OpportunityClaim)
 class OpportunityClaimAdmin(admin.ModelAdmin):
     list_display = ["get_username", "get_opp_name", "opportunity_access"]
+    inlines = [OpportunityClaimLimitInline]
 
     @admin.display(description="Opportunity Name")
     def get_opp_name(self, obj):
