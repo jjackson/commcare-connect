f99e6ba079d3f67ce14598448b8de6c071002dfc
hemant10yadav <hemant.10.yadav@gmail.com>
2024-09-25 11:28:05 +0530

    added performace table query and table

--- commcare_connect/program/helpers.py
+++ commcare_connect/program/helpers.py
@@ -3,15 +3,15 @@ from django.db.models import Avg, Count, DurationField, ExpressionWrapper, F, Ou
 from commcare_connect.opportunity.models import UserVisit, VisitValidationStatus
 from commcare_connect.program.models import ManagedOpportunity, Program
 
+FILTER_FOR_VALID_VISIT_DATE = ~Q(
+    opportunityaccess__uservisit__status__in=[
+        VisitValidationStatus.over_limit,
+        VisitValidationStatus.trial,
+    ]
+)
+
 
 def get_annotated_managed_opportunity(program: Program):
-    filter_for_valid__visit_date = ~Q(
-        opportunityaccess__uservisit__status__in=[
-            VisitValidationStatus.over_limit,
-            VisitValidationStatus.trial,
-        ]
-    )
-
     earliest_visits = (
         UserVisit.objects.filter(
             opportunity_access=OuterRef("opportunityaccess"),
@@ -35,7 +35,7 @@ def get_annotated_managed_opportunity(program: Program):
             ),
             workers_starting_delivery=Count(
                 "opportunityaccess__uservisit__user",
-                filter=filter_for_valid__visit_date,
+                filter=FILTER_FOR_VALID_VISIT_DATE,
                 distinct=True,
             ),
             percentage_conversion=F("workers_starting_delivery") / F("workers_invited") * 100,
@@ -43,13 +43,53 @@ def get_annotated_managed_opportunity(program: Program):
                 ExpressionWrapper(
                     Subquery(earliest_visits) - F("opportunityaccess__invited_date"), output_field=DurationField()
                 ),
-                filter=filter_for_valid__visit_date,
+                filter=FILTER_FOR_VALID_VISIT_DATE,
             ),
         )
         .prefetch_related(
             "opportunityaccess_set",
             "opportunityaccess_set__uservisit_set",
             "opportunityaccess_set__assessment_set",
+        )
+    )
+
+    return managed_opportunities
+
+
+def get_delivery_performance_report(program: Program, start_date, end_date):
+    date_filter = Q()
+
+    if start_date:
+        date_filter &= Q(opportunityaccess__uservisit__visit_date__gte=start_date)
+
+    if end_date:
+        date_filter &= Q(opportunityaccess__uservisit__visit_date__lte=end_date)
+
+    active_workers_filter = Q(FILTER_FOR_VALID_VISIT_DATE, date_filter)
+
+    managed_opportunities = (
+        ManagedOpportunity.objects.filter(program=program)
+        .order_by("start_date")
+        .prefetch_related(
+            "opportunityaccess_set",
+            "opportunityaccess_set__uservisit_set",
+            "opportunityaccess_set__completedwork_set",
+        )
+        .annotate(
+            total_workers_starting_delivery=Count(
+                "opportunityaccess__uservisit__user",
+                filter=FILTER_FOR_VALID_VISIT_DATE,
+                distinct=True,
+            ),
+            active_workers=Count(
+                "opportunityaccess__uservisit__user",
+                filter=active_workers_filter,
+                distinct=True,
+            ),
+            total_payment_units=Count("opportunityaccess__completedwork", distinct=True),
+            total_payement_since_start_date=Count("opportunityaccess__completedwork", distinct=True),
+            deliveries_per_day=F("total_payement_since_start_date") / F("active_workers"),
+            records_flagged_percentage=F("total_payment_units") / F("total_payement_since_start_date"),
         )
     )
 
