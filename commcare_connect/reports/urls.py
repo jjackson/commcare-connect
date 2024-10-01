a23b1688e29e8f59cf77ab79b83fd1c3ff50646b
Cory Zue <cory@coryzue.com>
2024-10-01 17:48:50 +0200

    fix url

--- commcare_connect/reports/urls.py
+++ commcare_connect/reports/urls.py
@@ -7,5 +7,5 @@ app_name = "reports"
 urlpatterns = [
     path("delivery_stats", views.delivery_stats_report, name="delivery_stats_report"),
     path("program_dashboard", views.program_dashboard_report, name="program_dashboard_report"),
-    path("api/visit-map-data/", views.visit_map_data, name="visit_map_data"),
+    path("api/visit_map_data/", views.visit_map_data, name="visit_map_data"),
 ]
