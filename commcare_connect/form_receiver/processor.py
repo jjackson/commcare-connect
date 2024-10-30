88a0f52cc01ed9ceffbfda506b8710403507d315
Pawan Verma <pverma@dimagi.com>
2024-10-30 16:35:02 +0530

    Add update_payment_accrued to form processor

--- commcare_connect/form_receiver/processor.py
+++ commcare_connect/form_receiver/processor.py
@@ -29,6 +29,7 @@ from commcare_connect.opportunity.models import (
     VisitValidationStatus,
 )
 from commcare_connect.opportunity.tasks import download_user_visit_attachments
+from commcare_connect.opportunity.visit_import import update_payment_accrued
 from commcare_connect.users.models import User
 
 LEARN_MODULE_JSONPATH = parse("$..module")
@@ -309,6 +310,7 @@ def process_deliver_unit(user, xform: XForm, app: CommCareApp, opportunity: Oppo
             completed_work_needs_save = True
         if completed_work_needs_save:
             completed_work.save()
+    update_payment_accrued(opportunity, [user.id])
     download_user_visit_attachments.delay(user_visit.id)
 
 
