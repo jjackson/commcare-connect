aaf94ff649aa6007383d6364ef4c331eec9495e2
Cory Zue <cory@coryzue.com>
2024-11-01 15:56:15 +0200

    add more data to the map properties

--- commcare_connect/reports/queries.py
+++ commcare_connect/reports/queries.py
@@ -19,9 +19,13 @@ def get_visit_map_queryset(base_queryset):
             ),
             location_str=KeyTextTransform("location", KeyTextTransform("metadata", F("form_json"))),
         )
-        .select_related("deliver_unit", "opportunity")
+        .select_related("deliver_unit", "opportunity", "opportunity__delivery_type", "opportunity__organization")
         .values(
             "opportunity_id",
+            "opportunity__delivery_type__name",
+            "opportunity__delivery_type__slug",
+            "opportunity__organization__slug",
+            "opportunity__organization__name",
             "xform_id",
             "visit_date",
             "username_connectid",
