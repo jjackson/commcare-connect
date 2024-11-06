2c2b8a0860a1bc7f90b78f4ca0dc7092feaf73b2
hemant10yadav <hemant.10.yadav@gmail.com>
2024-11-06 14:28:02 +0530

    removed the username option

--- commcare_connect/organization/tests/test_forms.py
+++ commcare_connect/organization/tests/test_forms.py
@@ -16,19 +16,17 @@ class TestAddMembersView:
 
     @pytest.mark.django_db
     @pytest.mark.parametrize(
-        "email_or_username, role, expected_status_code, create_user, expected_role, should_exist",
+        "email, role, expected_status_code, create_user, expected_role, should_exist",
         [
             ("testformember@example.com", "member", 302, True, "member", True),
             ("testforadmin@example.com", "admin", 302, True, "admin", True),
-            ("testforusername", "member", 302, True, "member", True),
-            ("testforusername", "admin", 302, True, "admin", True),
             ("nonexistent@example.com", "member", 302, False, None, False),
             ("existing@example.com", "admin", 302, True, "member", True),
         ],
     )
     def test_add_member(
         self,
-        email_or_username,
+        email,
         role,
         expected_status_code,
         create_user,
@@ -37,20 +35,15 @@ class TestAddMembersView:
         organization,
     ):
         if create_user:
-            user = UserFactory(
-                email=email_or_username if "@" in email_or_username else None,
-                username=email_or_username if "@" not in email_or_username else None,
-            )
+            user = UserFactory(email=email)
 
-            if email_or_username == "existing@example.com":
+            if email == "existing@example.com":
                 organization.members.add(user, through_defaults={"role": expected_role})
 
-        data = {"email_or_username": email_or_username, "role": role}
+        data = {"email": email, "role": role}
         response = self.client.post(self.url, data)
 
-        membership_filter = (
-            {"user__email": email_or_username} if "@" in email_or_username else {"user__username": email_or_username}
-        )
+        membership_filter = {"user__email": email}
 
         assert response.status_code == expected_status_code
         membership_exists = organization.memberships.filter(**membership_filter).exists()
