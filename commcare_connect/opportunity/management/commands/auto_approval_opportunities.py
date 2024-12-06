8314a3a61612bd137230c9127eaae5dcd2e7f464
Cal Ellowitz <calellowitz@gmail.com>
2024-12-05 22:25:58 -0500

    add arg to update payment

--- commcare_connect/opportunity/management/commands/auto_approval_opportunities.py
+++ commcare_connect/opportunity/management/commands/auto_approval_opportunities.py
@@ -17,9 +17,11 @@ class Command(BaseCommand):
         parser.add_argument(
             "--include-over-limit", action="store_true", help="Also run auto-approval logic on over limit works"
         )
+        parser.add_argument("--update-payment", action="store_true", help="Update payment accrued")
 
     def handle(self, *args, opp: int, **options):
         include_over_limit = options.get("include_over_limit", False)
+        update_payment = options.get("update_payment", False)
         excluded = [CompletedWorkStatus.rejected]
         if not include_over_limit:
             excluded.append(CompletedWorkStatus.over_limit)
@@ -30,7 +32,7 @@ class Command(BaseCommand):
             )
             for access in access_objects:
                 completed_works = access.completedwork_set.exclude(status__in=excluded)
-                update_status(completed_works, access, False)
+                update_status(completed_works, access, update_payment)
 
             self.stdout.write(self.style.SUCCESS(f"Successfully processed opportunity with id {opp}"))
 
