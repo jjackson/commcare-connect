f22f7dfde0d758561f1be9612c56a043b350b04e
hemant10yadav <hemant.10.yadav@gmail.com>
2025-01-09 18:45:18 +0530

    changed try catch imp

--- commcare_connect/form_receiver/views.py
+++ commcare_connect/form_receiver/views.py
@@ -1,11 +1,12 @@
 import logging
 
+from django.db import IntegrityError
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasReadWriteScope
 from rest_framework import parsers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from commcare_connect.form_receiver.processor import DuplicateFormException, process_xform
+from commcare_connect.form_receiver.processor import process_xform
 from commcare_connect.form_receiver.serializers import XFormSerializer
 
 logger = logging.getLogger(__name__)
@@ -22,7 +23,9 @@ class FormReceiver(APIView):
         xform = serializer.save()
         try:
             process_xform(xform)
-        except DuplicateFormException:
-            logger.info(f"Duplicate form with ID: {xform.id} received.")
-            pass
+        except IntegrityError as e:
+            if "unique_xform_entity_deliver_unit" in str(e):
+                logger.info(f"Duplicate form with ID: {xform.id} received.")
+            else:
+                raise
         return Response(status=status.HTTP_200_OK)
