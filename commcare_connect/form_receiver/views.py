810fc232ab8b0865cb9ec4f9dc603d521436c506
hemant10yadav <hemant.10.yadav@gmail.com>
2025-01-09 15:01:32 +0530

    logged the duplicate form

--- commcare_connect/form_receiver/views.py
+++ commcare_connect/form_receiver/views.py
@@ -1,10 +1,14 @@
+import logging
+
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasReadWriteScope
 from rest_framework import parsers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from commcare_connect.form_receiver.processor import process_xform
+from commcare_connect.form_receiver.processor import DuplicateFormException, process_xform
 from commcare_connect.form_receiver.serializers import XFormSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class FormReceiver(APIView):
@@ -16,5 +20,9 @@ class FormReceiver(APIView):
         serializer = XFormSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         xform = serializer.save()
-        process_xform(xform)
+        try:
+            process_xform(xform)
+        except DuplicateFormException:
+            logger.info(f"Duplicate form with ID: {xform.id} received.")
+            pass
         return Response(status=status.HTTP_200_OK)
