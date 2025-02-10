47634f26cff2130b5c526eb1c4a6cca389255cff
Pawan Verma <pverma@dimagi.com>
2025-02-10 15:33:12 +0530

    Add addtional metrics for the dashboard

--- commcare_connect/reports/helpers.py
+++ commcare_connect/reports/helpers.py
@@ -1,11 +1,16 @@
 import calendar
-from collections import defaultdict
 from datetime import datetime
 
-from django.db.models import Max, Sum
+from django.db.models import Avg, Count, DurationField, ExpressionWrapper, F, Max, OuterRef, Subquery, Sum
 from django.utils.timezone import make_aware
 
-from commcare_connect.opportunity.models import CompletedWork, CompletedWorkStatus, Payment
+from commcare_connect.opportunity.models import (
+    CompletedWork,
+    CompletedWorkStatus,
+    Payment,
+    UserVisit,
+    VisitValidationStatus,
+)
 
 
 def get_table_data_for_year_month(
@@ -17,27 +22,34 @@ def get_table_data_for_year_month(
     network_manager=None,
     opportunity=None,
 ):
-    filter_kwargs = {}
     if year is None:
         return []
+    filter_kwargs = {"opportunity_access__opportunity__is_test": False}
+    filter_kwargs_nm = {"invoice__opportunity__is_test": False}
     if delivery_type:
         filter_kwargs.update({"opportunity_access__opportunity__delivery_type__slug": delivery_type})
+        filter_kwargs_nm.update({"invoice__opportunity__delivery_type__slug": delivery_type})
     if program:
         filter_kwargs.update({"opportunity_access__opportunity__managedopportunity__program": program})
+        filter_kwargs_nm.update({"invoice__opportunity__managedopportunity__program": program})
     if network_manager:
         filter_kwargs.update({"opportunity_access__opportunity__organization": network_manager})
+        filter_kwargs_nm.update({"invoice__opportunity__organization": network_manager})
     if opportunity:
         filter_kwargs.update({"opportunity_access__opportunity": opportunity})
+        filter_kwargs_nm.update({"invoice__opportunity": opportunity})
 
     _, month_end = calendar.monthrange(year, month or 1)
     start_date = make_aware(datetime(year, month or 1, 1))
     end_date = make_aware(datetime(year, month or 12, month_end))
     data = []
 
-    user_set = defaultdict(set)
-    beneficiary_set = defaultdict(set)
-    service_count = defaultdict(int)
-
+    max_visit_date = (
+        UserVisit.objects.filter(completed_work_id=OuterRef("id"), status=VisitValidationStatus.approved)
+        .values_list("visit_date")
+        .order_by("-visit_date")[:1]
+    )
+    time_to_payment = ExpressionWrapper(F("payment_date") - Subquery(max_visit_date), output_field=DurationField())
     visit_data = (
         CompletedWork.objects.annotate(work_date=Max("uservisit__visit_date"))
         .filter(
@@ -47,57 +59,98 @@ def get_table_data_for_year_month(
             work_date__gte=start_date,
             work_date__lt=end_date,
         )
-        .select_related("opportunity_access__opportunity__delivery_type")
+        .values("opportunity_access__opportunity__delivery_type__name")
+        .annotate(
+            users=Count("opportunity_access__user_id", distinct=True),
+            beneficiaries=Count("entity_id", distinct=True),
+            service_count=Sum("saved_approved_count", default=0),
+            flw_amount_earned=Sum("saved_payment_accrued", default=0),
+            nm_amount_earned=Sum(
+                F("saved_approved_count")
+                * F("opportunity_access__opportunity__managedopportunity__org_pay_per_visit"),
+                default=0,
+            ),
+            avg_time_to_payment=Avg(time_to_payment, default=0),
+            max_time_to_payment=Max(time_to_payment, default=0),
+        )
     )
-    for v in visit_data:
-        delivery_type_name = "All"
-        if group_by_delivery_type and v.opportunity_access.opportunity.delivery_type:
-            delivery_type_name = v.opportunity_access.opportunity.delivery_type.name
-
-        user_set[delivery_type_name].add(v.opportunity_access.user_id)
-        beneficiary_set[delivery_type_name].add(v.entity_id)
-        service_count[delivery_type_name] += v.saved_approved_count
 
     payment_query = Payment.objects.filter(
-        **filter_kwargs,
-        opportunity_access__opportunity__is_test=False,
         date_paid__gte=start_date,
         date_paid__lt=end_date,
     )
 
-    approved_payment_data = (
-        payment_query.filter(confirmed=True)
+    user_count_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["users"] for item in visit_data
+    }
+    service_count_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["service_count"] for item in visit_data
+    }
+    avg_time_to_payment_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["avg_time_to_payment"]
+        for item in visit_data
+    }
+    max_time_to_payment_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["max_time_to_payment"]
+        for item in visit_data
+    }
+    flw_amount_earned_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["flw_amount_earned"] for item in visit_data
+    }
+    nm_amount_earned_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["nm_amount_earned"] for item in visit_data
+    }
+    flw_amount_paid = (
+        payment_query.filter(**filter_kwargs, confirmed=True)
         .values("opportunity_access__opportunity__delivery_type__name")
         .annotate(approved_sum=Sum("amount_usd", default=0))
     )
-    total_payment_data = payment_query.values("opportunity_access__opportunity__delivery_type__name").annotate(
-        total_sum=Sum("amount_usd", default=0)
-    )
-    approved_payment_dict = {
-        item["opportunity_access__opportunity__delivery_type__name"]: item["approved_sum"]
-        for item in approved_payment_data
+    flw_amount_paid_data = {
+        item["opportunity_access__opportunity__delivery_type__name"]: item["approved_sum"] for item in flw_amount_paid
     }
-    total_payment_dict = {
-        item["opportunity_access__opportunity__delivery_type__name"]: item["total_sum"] for item in total_payment_data
+    nm_amount_paid = (
+        payment_query.filter(**filter_kwargs_nm)
+        .values("invoice__opportunity__delivery_type__name")
+        .annotate(approved_sum=Sum("amount_usd", default=0))
+    )
+    nm_amount_paid_data = {
+        item["invoice__opportunity__delivery_type__name"]: item["approved_sum"] for item in nm_amount_paid
     }
 
-    for delivery_type_name in user_set.keys():
-        delivery_type_data = {
-            "delivery_type": delivery_type_name,
-            "month": (month, year),
-            "users": len(user_set[delivery_type_name]),
-            "services": service_count[delivery_type_name],
-            "approved_payments": approved_payment_dict.get(delivery_type_name, 0),
-            "total_payments": total_payment_dict.get(delivery_type_name, 0),
-            "beneficiaries": len(beneficiary_set[delivery_type_name]),
-        }
-        if delivery_type_name == "All":
-            delivery_type_data.update(
+    if group_by_delivery_type:
+        for delivery_type_name in user_count_data.keys():
+            nm_amount_earned = nm_amount_earned_data.get(delivery_type_name, 0)
+            nm_amount_paid = nm_amount_paid_data.get(delivery_type_name, 0)
+            data.append(
                 {
-                    "approved_payments": sum(approved_payment_dict.values()),
-                    "total_payments": sum(total_payment_dict.values()),
-                    "beneficiaries": len(beneficiary_set[delivery_type_name]),
+                    "delivery_type": delivery_type_name,
+                    "month": (month, year),
+                    "users": user_count_data[delivery_type_name],
+                    "services": service_count_data[delivery_type_name],
+                    "avg_time_to_payment": avg_time_to_payment_data.get(delivery_type_name, 0),
+                    "max_time_to_payment": max_time_to_payment_data.get(delivery_type_name, 0),
+                    "flw_amount_earned": flw_amount_earned_data.get(delivery_type_name, 0),
+                    "flw_amount_paid": flw_amount_paid_data.get(delivery_type_name, 0),
+                    "nm_amount_earned": nm_amount_earned,
+                    "nm_amount_paid": nm_amount_paid,
                 }
             )
-        data.append(delivery_type_data)
+
+    else:
+        nm_amount_earned = sum(nm_amount_earned_data.values())
+        nm_amount_paid = sum(nm_amount_paid_data.values())
+        data.append(
+            {
+                "delivery_type": "All",
+                "month": (month, year),
+                "users": sum(user_count_data.values()),
+                "services": sum(service_count_data.values()),
+                "avg_time_to_payment": sum(avg_time_to_payment_data.values()),
+                "max_time_to_payment": sum(max_time_to_payment_data.values()),
+                "flw_amount_earned": sum(flw_amount_earned_data.values()),
+                "flw_amount_paid": sum(flw_amount_paid_data.values()),
+                "nm_amount_earned": nm_amount_earned,
+                "nm_amount_paid": nm_amount_paid,
+            }
+        )
     return data
