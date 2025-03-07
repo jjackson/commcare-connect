ef38f4f49c2a32493d918d2615ca2c8d676272e6
Pawan Verma <pverma@dimagi.com>
2025-03-07 13:05:43 +0530

    Fix delivery stats report date filtering

--- commcare_connect/utils/datetime.py
+++ commcare_connect/utils/datetime.py
@@ -1,3 +1,4 @@
+import calendar
 import datetime
 
 from dateutil.relativedelta import relativedelta
@@ -16,3 +17,11 @@ def get_month_series(from_date: datetime.date, to_date: datetime.date):
         current_date += relativedelta(months=1)
         series.append(current_date)
     return series
+
+
+def get_start_end_dates_from_month_range(from_date: datetime.date, to_date: datetime.date):
+    start_date = datetime.date(from_date.year, from_date.month, 1)
+    start_time = datetime.datetime.combine(start_date, datetime.time.min, tzinfo=datetime.UTC)
+    end_date = datetime.date(to_date.year, to_date.month, calendar.monthrange(to_date.year, to_date.month)[1])
+    end_time = datetime.datetime.combine(end_date, datetime.time.max, tzinfo=datetime.UTC)
+    return start_time, end_time
