51460bd3365254f21ce08009949f03b8d6ab054e
Pawan Verma <pverma@dimagi.com>
2025-03-24 09:01:37 +0530

    Refactor split saved fields update and payment accrued update

--- commcare_connect/opportunity/utils/completed_work.py
+++ commcare_connect/opportunity/utils/completed_work.py
@@ -1,3 +1,5 @@
+from django.db.models import Sum
+
 from commcare_connect.opportunity.models import (
     CompletedWork,
     CompletedWorkStatus,
@@ -15,70 +17,83 @@ def update_status(completed_works, opportunity_access, compute_payment=True):
     If compute_payment is True, the saved fields related to completed/approved work and payments
     earned will also be saved against the model.
     """
-    payment_accrued = 0
-    for completed_work in completed_works:
-        payment_accrued += _update_status_set_saved_fields_and_get_payment_accrued(
-            completed_work, opportunity_access, compute_payment
-        )
-
+    update_status_and_set_saved_fields(completed_works, opportunity_access.opportunity, compute_payment)
     if compute_payment:
-        opportunity_access.payment_accrued = payment_accrued
+        opportunity_access.payment_accrued = (
+            CompletedWork.objects.filter(opportunity_access=opportunity_access)
+            .aggregate(payment_accrued=Sum("saved_payment_accrued"))
+            .get("payment_accrued", 0)
+            or 0
+        )
         opportunity_access.save()
 
 
-def _update_status_set_saved_fields_and_get_payment_accrued(completed_work, opportunity_access, compute_payment):
-    completed_count = completed_work.completed_count
-    if completed_count < 1:
-        return 0
+def update_status_and_set_saved_fields(completed_works, opportunity, compute_payment):
+    to_update = []
+    for completed_work in completed_works:
+        # completed_count = completed_approved_count[completed_work.id]["completed"]
+        completed_count = completed_work.completed_count
+        if completed_count < 1:
+            continue
 
-    amount_accrued = 0
-    made_changes = False
-    if opportunity_access.opportunity.auto_approve_payments:
-        visits = completed_work.uservisit_set.values_list("status", "reason", "review_status")
-        if any(status == VisitValidationStatus.rejected for status, *_ in visits):
-            completed_work.status = CompletedWorkStatus.rejected
-            completed_work.reason = "\n".join(reason for _, reason, _ in visits if reason)
-        elif all(status == VisitValidationStatus.approved for status, *_ in visits):
-            completed_work.status = CompletedWorkStatus.approved
+        amount_accrued = 0
+        updated = False
+        if opportunity.auto_approve_payments:
+            visits = completed_work.uservisit_set.values_list("status", "reason", "review_status")
+            if any(status == VisitValidationStatus.rejected for status, *_ in visits):
+                completed_work.status = CompletedWorkStatus.rejected
+                completed_work.reason = "\n".join(reason for _, reason, _ in visits if reason)
+            elif all(status == VisitValidationStatus.approved for status, *_ in visits):
+                completed_work.status = CompletedWorkStatus.approved
 
-        if (
-            opportunity_access.opportunity.managed
-            and not all(review_status == VisitReviewStatus.agree for *_, review_status in visits)
-            and completed_work.status == CompletedWorkStatus.approved
-        ):
-            completed_work.status = CompletedWorkStatus.pending
+            if (
+                opportunity.managed
+                and not all(review_status == VisitReviewStatus.agree for *_, review_status in visits)
+                and completed_work.status == CompletedWorkStatus.approved
+            ):
+                completed_work.status = CompletedWorkStatus.pending
 
-        made_changes = True
+            updated = True
 
-    if compute_payment:
-        approved_count = completed_work.approved_count
+        if compute_payment:
+            approved_count = completed_work.approved_count
 
-        amount_accrued = amount_accrued_usd = org_amount_accrued = org_amount_accrued_usd = 0
-        if approved_count > 0 and completed_work.status == CompletedWorkStatus.approved:
-            from commcare_connect.opportunity.visit_import import get_exchange_rate
+            amount_accrued = amount_accrued_usd = org_amount_accrued = org_amount_accrued_usd = 0
+            if approved_count > 0 and completed_work.status == CompletedWorkStatus.approved:
+                from commcare_connect.opportunity.visit_import import get_exchange_rate
 
-            amount_accrued = approved_count * completed_work.payment_unit.amount
-            exchange_rate = get_exchange_rate(
-                opportunity_access.opportunity.currency, completed_work.status_modified_date
-            )
-            amount_accrued_usd = amount_accrued / exchange_rate
-            # if it's a managed opportunity we also need to update the org payment amounts
-            if opportunity_access.opportunity.managed:
-                org_amount_accrued = approved_count * opportunity_access.managed_opportunity.org_pay_per_visit
-                org_amount_accrued_usd = org_amount_accrued / exchange_rate
+                amount_accrued = approved_count * completed_work.payment_unit.amount
+                exchange_rate = get_exchange_rate(opportunity.currency, completed_work.status_modified_date)
+                amount_accrued_usd = amount_accrued / exchange_rate
+                # if it's a managed opportunity we also need to update the org payment amounts
+                if opportunity.managed:
+                    org_amount_accrued = approved_count * opportunity.org_pay_per_visit
+                    org_amount_accrued_usd = org_amount_accrued / exchange_rate
 
-        completed_work.saved_completed_count = completed_count
-        completed_work.saved_approved_count = approved_count
-        completed_work.saved_payment_accrued = amount_accrued
-        completed_work.saved_payment_accrued_usd = amount_accrued_usd
-        completed_work.saved_org_payment_accrued = org_amount_accrued
-        completed_work.saved_org_payment_accrued_usd = org_amount_accrued_usd
-        made_changes = True
+            completed_work.saved_completed_count = completed_count
+            completed_work.saved_approved_count = approved_count
+            completed_work.saved_payment_accrued = amount_accrued
+            completed_work.saved_payment_accrued_usd = amount_accrued_usd
+            completed_work.saved_org_payment_accrued = org_amount_accrued
+            completed_work.saved_org_payment_accrued_usd = org_amount_accrued_usd
+            updated = True
 
-    if made_changes:
-        completed_work.save()
+        if updated:
+            to_update.append(completed_work)
 
-    return amount_accrued
+    CompletedWork.objects.bulk_update(
+        to_update,
+        fields=[
+            "status",
+            "status_modified_date",
+            "saved_completed_count",
+            "saved_approved_count",
+            "saved_payment_accrued",
+            "saved_payment_accrued_usd",
+            "saved_org_payment_accrued",
+            "saved_org_payment_accrued_usd",
+        ],
+    )
 
 
 def update_work_payment_date(access: OpportunityAccess):
