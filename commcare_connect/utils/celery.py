2e2ab26f6831c225f4c1869bcb58f88c75da6a35
Sravan Reddy <sravfeyn@gmail.com>
2025-04-07 15:56:39 +0530

    make constants

--- commcare_connect/utils/celery.py
+++ commcare_connect/utils/celery.py
@@ -1,5 +1,9 @@
+CELERY_TASK_SUCCESS = "SUCCESS"
+CELERY_TASK_IN_PROGRESS = "PROGRESS"
+
+
 def set_task_progress(task, message, is_complete=False):
-    task.update_state(state="SUCCESS" if is_complete else "PROGRESS", meta={"message": message})
+    task.update_state(state=CELERY_TASK_SUCCESS if is_complete else CELERY_TASK_IN_PROGRESS, meta={"message": message})
 
 
 def get_task_progress_message(task):
