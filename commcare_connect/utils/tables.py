f76e6ce60d1a06fcb419d420d77a76e72339e5f6
Sravan Reddy <sravfeyn@gmail.com>
2025-04-22 13:08:13 +0530

    Add sorting,pagination to base-table

--- commcare_connect/utils/tables.py
+++
@@ -1,9 +0,0 @@
-import django_tables2 as tables
-
-
-class BaseTailwindTable(tables.Table):
-    # Todo; this can be set using DJANGO_TABLES2_TEMPLATE
-    #   once the UI migration is complete
-
-    class Meta:
-        template_name = "tailwind/base_table.html"
