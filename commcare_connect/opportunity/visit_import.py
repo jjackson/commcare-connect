8d2d9a0bc982f4e944f9c47a581abebffad712c8
Pawan Verma <pverma@dimagi.com>
2025-05-24 16:03:51 +0530

    Fix add check active is string

--- commcare_connect/opportunity/visit_import.py
+++ commcare_connect/opportunity/visit_import.py
@@ -518,7 +518,7 @@ class RowData:
         error_message = "Active status must be 'yes' or 'no'"
         index = _get_header_index(self.headers, ACTIVE_COL)
         active = self.row[index]
-        if not active:
+        if not active or not isinstance(active, str):
             raise InvalidValueError(error_message)
         active = active.lower().strip()
         if active not in ["yes", "no"]:
