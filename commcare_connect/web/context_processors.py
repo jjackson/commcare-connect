6909700af32f302bad151c50860aa2a153c8c853
hemant10yadav <hemant.10.yadav@gmail.com>
2025-05-28 15:44:45 +0530

    added default page size

--- commcare_connect/web/context_processors.py
+++ commcare_connect/web/context_processors.py
@@ -1,8 +1,9 @@
-from commcare_connect.utils.tables import PAGE_SIZE_OPTIONS
+from commcare_connect.utils.tables import PAGE_SIZE_OPTIONS, DEFAULT_PAGE_SIZE
 
 
 def page_settings(request):
     """Expose global page size settings to templates."""
     return {
         "PAGE_SIZE_OPTIONS": PAGE_SIZE_OPTIONS,
+        "DEFAULT_PAGE_SIZE": DEFAULT_PAGE_SIZE,
     }
