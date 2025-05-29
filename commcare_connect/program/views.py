51dd168649c1225ccad53317cbb313da0dc92c4b
hemant10yadav <hemant.10.yadav@gmail.com>
2025-05-29 16:39:18 +0530

    fixed urls

--- commcare_connect/program/views.py
+++ commcare_connect/program/views.py
@@ -23,7 +23,7 @@ from commcare_connect.opportunity.views import OpportunityInit
 from commcare_connect.organization.decorators import (
     org_admin_required,
     org_program_manager_required,
-    org_viewer_required,
+    org_viewer_required, org_member_required,
 )
 from commcare_connect.organization.models import Organization
 from commcare_connect.program.forms import ManagedOpportunityInitForm, ProgramForm
@@ -262,7 +262,7 @@ class DeliveryPerformanceTableView(ProgramManagerMixin, SingleTableView):
         return get_delivery_performance_report(program, start_date, end_date)
 
 
-@org_viewer_required
+@org_member_required
 def program_home(request, org_slug):
     org = Organization.objects.get(slug=org_slug)
     if is_program_manager(request):
@@ -318,13 +318,13 @@ def program_manager_home(request, org):
     )
 
     pending_payments = _make_recent_activity_data(
-        pending_payments_data, org.slug, "opportunity:worker_list", {"active_tab": "payments"}, small_text=True
+        pending_payments_data, org.slug, "opportunity:invoice_list", small_text=True
     )
 
     organizations = Organization.objects.exclude(pk=org.pk)
     recent_activities = [
         {"title": "Pending Review", "rows": pending_review},
-        {"title": "Pending Payments", "rows": pending_payments},
+        {"title": "Pending Invoices", "rows": pending_payments},
     ]
 
     context = {
