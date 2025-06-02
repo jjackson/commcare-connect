98a285cd04ae06ff35b073d2fbfb5856a8e292da
Cory Zue <cory@coryzue.com>
2025-06-02 15:28:41 +0200

    add created_by / modified_by fields

--- commcare_connect/deid/config.py
+++ commcare_connect/deid/config.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass
 
 from django.db import models
 
-from commcare_connect.opportunity.models import CompletedWork, UserInvite, UserVisit
+from commcare_connect.opportunity.models import CommCareApp, CompletedWork, Opportunity, UserInvite, UserVisit
+from commcare_connect.program.models import Program, ProgramApplication
 from commcare_connect.users.models import ConnectIDUserLink, User
 
 STRATEGY_DROP_FIELD = "drop_field"
@@ -47,6 +48,22 @@ def get_fields_to_anonymize() -> list[AnonymizationConfig]:
         UserInvite: [
             "phone_number",
             "message_sid",
+        ],
+        CommCareApp: [
+            "created_by",
+            "modified_by",
+        ],
+        Program: [
+            "created_by",
+            "modified_by",
+        ],
+        ProgramApplication: [
+            "created_by",
+            "modified_by",
+        ],
+        Opportunity: [
+            "created_by",
+            "modified_by",
         ],
     }
     return [
