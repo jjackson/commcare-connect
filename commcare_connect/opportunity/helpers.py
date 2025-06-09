ffb2329c60b80aa47db0cde35c1021128f154e61
Sravan Reddy <sravfeyn@gmail.com>
2025-06-09 11:57:28 +0530

    Move python cacls to db

--- commcare_connect/opportunity/helpers.py
+++ commcare_connect/opportunity/helpers.py
@@ -35,7 +35,6 @@ from commcare_connect.opportunity.models import (
     OpportunityClaim,
     OpportunityClaimLimit,
     Payment,
-    PaymentUnit,
     UserInvite,
     UserInviteStatus,
     UserVisit,
@@ -315,25 +314,36 @@ def get_annotated_opportunity_access_deliver_status(opportunity: Opportunity):
 
 
 def get_payment_report_data(opportunity: Opportunity):
-    payment_units = PaymentUnit.objects.filter(opportunity=opportunity)
     PaymentReportData = namedtuple(
         "PaymentReportData", ["payment_unit", "approved", "user_payment_accrued", "nm_payment_accrued"]
+    )
+
+    report_data_qs = (
+        CompletedWork.objects.filter(opportunity_access__opportunity=opportunity, status=CompletedWorkStatus.approved)
+        .values("payment_unit__name", amount=F("payment_unit__amount"))
+        .annotate(
+            completed_work_count=Count("id"),
+            total_saved_approved=Sum("saved_approved_count"),
+        )
+        .order_by("payment_unit__name")
     )
     data = []
     total_user_payment_accrued = 0
     total_nm_payment_accrued = 0
-    for payment_unit in payment_units:
-        completed_works = CompletedWork.objects.filter(
-            opportunity_access__opportunity=opportunity, status=CompletedWorkStatus.approved, payment_unit=payment_unit
+    org_pay_per_visit = opportunity.managedopportunity.org_pay_per_visit
+    for group in report_data_qs:
+        user_payment_accrued = group["total_saved_approved"] * group["amount"]
+        nm_payment_accrued = group["completed_work_count"] * org_pay_per_visit
+        data.append(
+            PaymentReportData(
+                group["payment_unit__name"],
+                group["completed_work_count"],
+                user_payment_accrued,
+                nm_payment_accrued,
+            )
         )
-        completed_work_count = len(completed_works)
-        user_payment_accrued = sum([cw.payment_accrued for cw in completed_works])
-        nm_payment_accrued = completed_work_count * opportunity.managedopportunity.org_pay_per_visit
         total_user_payment_accrued += user_payment_accrued
         total_nm_payment_accrued += nm_payment_accrued
-        data.append(
-            PaymentReportData(payment_unit.name, completed_work_count, user_payment_accrued, nm_payment_accrued)
-        )
     return data, total_user_payment_accrued, total_nm_payment_accrued
 
 
