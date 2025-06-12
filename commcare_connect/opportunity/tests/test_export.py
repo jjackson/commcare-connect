5913983b34937defa5f5d989d627bd9b0e50bba0
Pawan Verma <pverma@dimagi.com>
2025-06-12 10:58:54 +0530

    Add test

--- commcare_connect/opportunity/tests/test_export.py
+++ commcare_connect/opportunity/tests/test_export.py
@@ -67,6 +67,48 @@ def test_export_user_visit_data(mobile_user_with_connect_link):
     )
 
 
+def test_export_user_visit_data_no_flatten(mobile_user_with_connect_link):
+    opportunity = OpportunityFactory()
+    deliver_units = DeliverUnitFactory.create_batch(2, app=opportunity.deliver_app)
+    date1 = now()
+    date2 = date1 + timedelta(minutes=10)
+    form_json_1 = {"form": {"name": "test_form1"}}
+    form_json_1_string = '{""form"": {""name"": ""test_form1""}}'
+    form_json_2 = {"form": {"name": "test_form2", "group": {"q": "b"}}}
+    form_json_2_string = '{""form"": {""name"": ""test_form2"", ""group"": {""q"": ""b""}}}'
+    UserVisit.objects.bulk_create(
+        [
+            UserVisit(
+                opportunity=opportunity,
+                user=mobile_user_with_connect_link,
+                visit_date=date1,
+                deliver_unit=deliver_units[0],
+                form_json=form_json_1,
+            ),
+            UserVisit(
+                opportunity=opportunity,
+                user=mobile_user_with_connect_link,
+                visit_date=date2,
+                deliver_unit=deliver_units[1],
+                entity_id="abc",
+                entity_name="A B C",
+                form_json=form_json_2,
+            ),
+        ]
+    )
+    exporter = UserVisitExporter(opportunity, False)
+    dataset = exporter.get_dataset(DateRanges.LAST_30_DAYS, [])
+    username = mobile_user_with_connect_link.username
+    name = mobile_user_with_connect_link.name
+    assert dataset.export("csv") == (
+        "Visit ID,Visit date,Status,Username,Name of User,Unit Name,Rejected Reason,"
+        "Duration,Entity ID,Entity Name,Flags,form_json\r\n"
+        f',{date1.isoformat()},Pending,{username},{name},{deliver_units[0].name},,,,,,"{form_json_1_string}"\r\n'
+        f",{date2.isoformat()},Pending,{username},{name},{deliver_units[1].name},,,abc,A B C,,"
+        f'"{form_json_2_string}"\r\n'
+    )
+
+
 def _get_prepared_dataset_for_user_status_test(data):
     headers = (
         "Name",
