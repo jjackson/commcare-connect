620a1d8daad7731cbeeeeb8df55a8a287c31a6e5
Pawan Verma <pverma@dimagi.com>
2025-06-19 18:33:40 +0530

    Add better display for HQServer

--- commcare_connect/opportunity/models.py
+++ commcare_connect/opportunity/models.py
@@ -22,6 +22,9 @@ class HQServer(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.name} ({self.url})"
+
 
 class CommCareApp(BaseModel):
     organization = models.ForeignKey(
