77cae3e10b59182005078b603c7a6ac06f70f0e9
Pawan Verma <pverma@dimagi.com>
2025-07-08 16:19:25 +0530

    Add seperate javascript files for libraries

--- commcare_connect/static/js/vendors.js
+++ commcare_connect/static/js/vendors.js
@@ -1,19 +1,5 @@
 import './htmx';
 import 'htmx.org/dist/ext/loading-states';
 
-import flatpickr from 'flatpickr';
-import monthSelectPlugin from 'flatpickr/dist/esm/plugins/monthSelect';
-import 'flatpickr/dist/flatpickr.css';
-import 'flatpickr/dist/plugins/monthSelect/style.css';
-window.monthPickr = (id, flatPickrConfig = {}, monthPluginConfig = {}) =>
-  flatpickr(id, {
-    plugins: [new monthSelectPlugin({ ...monthPluginConfig })],
-    ...flatPickrConfig,
-  });
-
-import Alpine from 'alpinejs';
-import Tooltip from '@ryangjchandler/alpine-tooltip';
-Alpine.plugin(Tooltip);
-window.Alpine = Alpine;
-window.Alpine.start();
-import 'tippy.js/dist/tippy.css';
+import './alpine';
+import './flatpickr';
