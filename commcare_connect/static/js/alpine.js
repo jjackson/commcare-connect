3d81f1efe71249e2c53138345462dbbefddc5a05
hemant10yadav <hemant.10.yadav@gmail.com>
2025-08-04 08:19:07 +0530

    added project alpine.js

--- commcare_connect/static/js/alpine.js
+++ commcare_connect/static/js/alpine.js
@@ -2,5 +2,6 @@ import Alpine from 'alpinejs';
 import Tooltip from '@ryangjchandler/alpine-tooltip';
 Alpine.plugin(Tooltip);
 window.Alpine = Alpine;
+import './project_alpine.js';
 window.Alpine.start();
 import 'tippy.js/dist/tippy.css';
