5b74c0357756354fc3ffd260c2d509813005ae23
hemant10yadav <hemant.10.yadav@gmail.com>
2025-08-04 08:19:07 +0530

    added project alpine.js

---
+++ commcare_connect/static/js/project_alpine.js
@@ -0,0 +1,34 @@
+/**
+ * Centralized registry for custom Alpine.js components used across the project.
+ *
+ * All components are initialized within the `alpine:init` event to ensure
+ * they are registered before Alpine processes the DOM. This prevents
+ * reference errors and ensures reliable initialization of dynamic UI elements.
+ */
+
+document.addEventListener('alpine:init', () => {
+  // used in user_visit_verification_table.html
+  Alpine.data('visitVerification', (initialTab, defaultPage) => ({
+    activeTab: initialTab,
+    page: defaultPage,
+    selected: [],
+    showApproveModal: false,
+    showRejectModal: false,
+
+    updateUrlAndRequest() {
+      const url = new URL(window.location);
+      const formData = new FormData(this.$refs.visitForm);
+      const params = new URLSearchParams();
+
+      for (let [key, value] of formData.entries()) {
+        if (value && String(value).trim() !== '') {
+          params.set(key, value);
+        }
+      }
+
+      url.search = params.toString();
+      window.history.pushState({}, '', url.toString());
+      this.$dispatch('reload_table');
+    },
+  }));
+});
