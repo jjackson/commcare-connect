cab2b661569fadf3dac29996b2ef4753bb1fdef7
hemant10yadav <hemant.10.yadav@gmail.com>
2025-08-11 10:35:16 +0530

    added a common modal

--- commcare_connect/static/js/alpine.js
+++ commcare_connect/static/js/alpine.js
@@ -2,6 +2,5 @@ import Alpine from 'alpinejs';
 import Tooltip from '@ryangjchandler/alpine-tooltip';
 Alpine.plugin(Tooltip);
 window.Alpine = Alpine;
-import './project_alpine.js';
 window.Alpine.start();
 import 'tippy.js/dist/tippy.css';
