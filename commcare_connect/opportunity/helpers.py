451b57b22c405367d804ef76c7e2e56db6b2d699
Sravan Reddy <sravfeyn@gmail.com>
2025-08-20 09:43:45 +0530

    Default to lite version for tests

--- commcare_connect/opportunity/helpers.py
+++ commcare_connect/opportunity/helpers.py
@@ -390,6 +390,24 @@ class TieredQueryset:
         return self
 
 
+def get_opportunity_list_data_lite(org, program_manager=False):
+    today = now().date()
+    base_filter = Q(organization=org)
+    if program_manager:
+        base_filter |= Q(managedopportunity__program__organization=org)
+
+    queryset = Opportunity.objects.filter(base_filter).annotate(
+        program=F("managedopportunity__program__name"),
+        status=Case(
+            When(Q(active=True) & Q(end_date__gte=today), then=Value(0)),  # Active
+            When(Q(active=True) & Q(end_date__lt=today), then=Value(1)),  # Ended
+            default=Value(2),  # Inactive
+            output_field=IntegerField(),
+        ),
+    )
+    return queryset
+
+
 class OpportunityData:
     def __init__(self, org, is_program_manager):
         self.org = org
