de53bb7e7c7e82b82e23611dd5bbd355683ed6d4
Pawan Verma <pverma@dimagi.com>
2025-10-01 15:57:26 +0200

    Add Opportunity Data API

--- commcare_connect/data_export/views.py
+++ commcare_connect/data_export/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from oauth2_provider.contrib.rest_framework.permissions import TokenHasScope
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
@@ -8,6 +9,7 @@ from commcare_connect.data_export.serializer import (
     OrganizationDataExportSerializer,
     ProgramDataExportSerializer,
 )
+from commcare_connect.opportunity.api.serializers import BaseOpportunitySerializer
 from commcare_connect.opportunity.models import Opportunity
 from commcare_connect.organization.models import Organization
 from commcare_connect.program.models import Program
@@ -28,3 +30,10 @@ class ProgramOpportunityOrganizationDataView(BaseDataExportView):
         opp_data = OpportunityDataExportSerializer(opportunities, many=True).data
         program_data = ProgramDataExportSerializer(programs, many=True).data
         return JsonResponse({"organizations": org_data, "opportunities": opp_data, "programs": program_data})
+
+
+class OpportunityDataView(RetrieveAPIView, BaseDataExportView):
+    serializer_class = BaseOpportunitySerializer
+
+    def get_object(self):
+        return Opportunity.objects.get(id=self.kwargs.get("opp_id"), organization__memberships__user=self.request.user)
