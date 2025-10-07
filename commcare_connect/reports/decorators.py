7fea9747e09fab25a73d5ad7b1444ffce858407e
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2025-10-07 17:09:46 +0530

    added a new file for permission const

--- commcare_connect/reports/decorators.py
+++ commcare_connect/reports/decorators.py
@@ -4,19 +4,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 
-KPI_PERMISSION_NAME = "users.kpi_report_access"
+from commcare_connect.utils.permission_const import KPI_REPORT_ACCESS
 
 
 class KPIReportMixin(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
-        return self.request.user.has_perm(KPI_PERMISSION_NAME)
+        return self.request.user.has_perm(KPI_REPORT_ACCESS)
 
 
 def kpi_report_access_required(view_func):
     @login_required
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if request.user.has_perm(KPI_PERMISSION_NAME):
+        if request.user.has_perm(KPI_REPORT_ACCESS):
             return view_func(request, *args, **kwargs)
         raise PermissionDenied()
 
