3e631c4bad41b8b4e1ad14efaa1dea24f09bede3
Pawan Verma <pverma@dimagi.com>
2025-10-08 18:35:32 +0530

    Use underscores instead of hyphens

--- commcare_connect/data_export/urls.py
+++ commcare_connect/data_export/urls.py
@@ -4,18 +4,18 @@ from commcare_connect.data_export import views
 
 app_name = "data_export"
 urlpatterns = [
-    path("opp-org-program-list/", views.ProgramOpportunityOrganizationDataView.as_view(), name="opp_org_program_list"),
+    path("opp_org_program_list/", views.ProgramOpportunityOrganizationDataView.as_view(), name="opp_org_program_list"),
     path("opportunity/<int:opp_id>/", views.SingleOpportunityDataView.as_view(), name="opportunity_data"),
-    path("opportunity/<int:opp_id>/user-data/", views.OpportunityUserDataView.as_view(), name="opportunity_user_data"),
-    path("opportunity/<int:opp_id>/user-visits/", views.UserVisitDataView.as_view(), name="user_visit_data"),
+    path("opportunity/<int:opp_id>/user_data/", views.OpportunityUserDataView.as_view(), name="opportunity_user_data"),
+    path("opportunity/<int:opp_id>/user_visits/", views.UserVisitDataView.as_view(), name="user_visit_data"),
     path(
-        "opportunity/<int:opp_id>/completed-works/", views.CompletedWorkDataView.as_view(), name="completed_work_data"
+        "opportunity/<int:opp_id>/completed_works/", views.CompletedWorkDataView.as_view(), name="completed_work_data"
     ),
     path("opportunity/<int:opp_id>/payment/", views.PaymentDataView.as_view(), name="payment_data"),
     path("opportunity/<int:opp_id>/invoice/", views.InvoiceDataView.as_view(), name="invoice_data"),
     path("opportunity/<int:opp_id>/assessment/", views.AssessmentDataView.as_view(), name="assessment_data"),
     path(
-        "opportunity/<int:opp_id>/completed-module/",
+        "opportunity/<int:opp_id>/completed_module/",
         views.CompletedModuleDataView.as_view(),
         name="completed_module_data",
     ),
