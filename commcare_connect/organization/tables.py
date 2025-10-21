f675cc2ab477dcdcb2a74769ae6c25b7aba69a0c
Charl Smit <csmit@dimagi.com>
2025-10-21 11:25:28 +0200

    Add select column

--- commcare_connect/organization/tables.py
+++ commcare_connect/organization/tables.py
@@ -7,8 +7,28 @@ from commcare_connect.organization.models import UserOrganizationMembership
 
 
 class OrgMemberTable(tables.Table):
+    select = tables.CheckBoxColumn(
+        accessor="pk",
+        attrs={
+            "th__input": {
+                "@click": "toggleSelectAll()",
+                "x-model": "selectAll",
+                "name": "select_all",
+                "type": "checkbox",
+                "class": "checkbox",
+            },
+            "td__input": {
+                "x-model": "selected",
+                "@click.stop": "",  # used to stop click propagation
+                "name": "row_select",
+                "type": "checkbox",
+                "class": "checkbox",
+                "value": lambda record: record.pk,
+                "id": lambda record: f"row_checkbox_{record.pk}",
+            },
+        },
+    )
     use_view_url = True
-
     index = IndexColumn()
     user = columns.Column(verbose_name="member", accessor="user__email")
     role = tables.Column()
@@ -16,7 +36,7 @@ class OrgMemberTable(tables.Table):
     class Meta:
         model = UserOrganizationMembership
         fields = ("role", "user")
-        sequence = ("index", "user", "role")
+        sequence = ("select", "index", "user", "role")
 
     def render_role(self, value):
         return format_html("<div class=' underline underline-offset-4'>{}</div>", value)
