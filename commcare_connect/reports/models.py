e9b3d564ebfd141c417ccd1568c152f1d3611950
Pawan Verma <pverma@dimagi.com>
2025-11-04 15:27:58 +0530

    Add UserAnalyticsData model and migration

---
+++ commcare_connect/reports/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from commcare_connect.users.models import User
+
+
+class UserAnalyticsData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    has_accepted_opp = models.DateTimeField(null=True)
+    has_started_learning = models.DateTimeField(null=True)
+    has_completed_learning = models.DateTimeField(null=True)
+    has_completed_assessment = models.DateTimeField(null=True)
+    has_claimed_job = models.DateTimeField(null=True)
+    has_started_job = models.DateTimeField(null=True)
+    has_paid = models.DateTimeField(null=True)
+    has_completed_opp = models.DateTimeField(null=True)
+    has_completed_multiple_opps = models.DateTimeField(null=True)
+    has_offered_multiple_opps = models.DateTimeField(null=True)
+    has_accepted_multiple_opps = models.DateTimeField(null=True)
