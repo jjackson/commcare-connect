93114082b6aa2fe36da35231426b782c4f643767
Charl Smit <csmit@dimagi.com>
2025-11-06 09:25:31 +0200

    Add attr for governing plugin

--- commcare_connect/static/js/tomselect.js
+++ commcare_connect/static/js/tomselect.js
@@ -4,8 +4,12 @@ import TomSelect from 'tom-select';
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('[data-tomselect]').forEach((el) => {
     el.removeAttribute('class');
+    let plugings = [];
+    if (!el.hasAttribute('data-tomselect:no-remove-button')) {
+      plugings.push('remove_button');
+    }
     new TomSelect(el, {
-      plugins: ['remove_button'],
+      plugins: plugings,
     });
   });
 });
