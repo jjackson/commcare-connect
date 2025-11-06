dddeb3d26c40c886e0e0a5dbad3f8a097f9baff2
Pawan Verma <pverma@dimagi.com>
2025-11-06 18:08:38 +0530

    Refactor combine queries and reorganize

--- commcare_connect/reports/management/commands/backfill_user_analytics_data.py
+++ commcare_connect/reports/management/commands/backfill_user_analytics_data.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-from django.db.models import Case, Count, DateTimeField, F, Max, Min, Q, Sum, When
+from django.db.models import Case, Count, DateTimeField, IntegerField, Max, Min, Q, Sum, Value, When
 from django.db.models.lookups import GreaterThanOrEqual
 
 from commcare_connect.opportunity.models import CompletedWorkStatus, OpportunityAccess
@@ -14,11 +14,21 @@ class Command(BaseCommand):
         users = User.objects.filter(username__isnull=False, email__isnull=True)
         access_objects = (
             OpportunityAccess.objects.filter(user__in=users)
-            .order_by("invited_date")
-            .values("user_id")
             .annotate(
                 max_total=Sum("opportunity__paymentunit__max_total"),
                 max_total_done=Count("completedwork__id", Q(completedwork__status=CompletedWorkStatus.approved.value)),
+            )
+            .values("user_id")
+            .annotate(
+                total=Count("id", distinct=True),
+                accepted_count=Count("id", Q(accepted=True), distinct=True),
+                completed_count=Sum(
+                    Case(
+                        When(Q(GreaterThanOrEqual("max_total_done", "max_total"), accepted=True), then=Value(1)),
+                        default=Value(0),
+                        output_field=IntegerField(),
+                    )
+                ),
             )
             .annotate(
                 has_opp_invite=Max("invited_date"),
@@ -37,6 +47,24 @@ class Command(BaseCommand):
                     default=None,
                     output_field=DateTimeField(),
                 ),
+                has_offered_multiple_opps=Case(
+                    When(Q(total__gt=1), then=Max("invited_date")),
+                    default=None,
+                    output_field=DateTimeField(),
+                ),
+                has_accepted_multiple_opps=Case(
+                    When(Q(accepted_count__gt=1), then=Max("invited_date")),
+                    default=None,
+                    output_field=DateTimeField(),
+                ),
+                has_completed_multiple_opps=Case(
+                    When(
+                        Q(completed_count__gt=1),
+                        then=Max("completedwork__status_modified_date"),
+                    ),
+                    default=None,
+                    output_field=DateTimeField(),
+                ),
             )
             .values(
                 "user_id",
@@ -49,41 +77,14 @@ class Command(BaseCommand):
                 "has_started_job",
                 "has_paid",
                 "has_completed_opp",
+                "has_completed_multiple_opps",
+                "has_offered_multiple_opps",
+                "has_accepted_multiple_opps",
             )
         )
-        multiple_opp_calculation_qs = (
-            OpportunityAccess.objects.filter(user__in=users)
-            .values("user_id")
-            .annotate(total=Count("*"), invited_date_d=Max("invited_date"))
-            .filter(total__gt=1)
-            .order_by("invited_date")
-            .annotate(
-                has_completed_multiple_opps=F("invited_date_d"),
-                has_offered_multiple_opps=F("invited_date_d"),
-                has_accepted_multiple_opps=F("invited_date_d"),
-            )
-            .values(
-                "user_id", "has_completed_multiple_opps", "has_offered_multiple_opps", "has_accepted_multiple_opps"
-            )
-        )
-
-        user_map = {}
-        for access in access_objects:
-            user_id = access.pop("user_id")
-            if user_id is not None:
-                user_map[user_id] = {**access, "user_id": user_id}
-
-        for data in multiple_opp_calculation_qs:
-            user_id = data.pop("user_id")
-            if user_id is not None:
-                user_map[user_id] = {**data, "user_id": user_id}
-
-        to_create = []
-        for user_data in user_map.values():
-            to_create.append(UserAnalyticsData(**user_data))
 
         UserAnalyticsData.objects.bulk_create(
-            to_create,
+            [UserAnalyticsData(**data) for data in access_objects],
             update_conflicts=True,
             update_fields=[
                 "has_opp_invite",
