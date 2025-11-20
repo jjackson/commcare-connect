5bb0b8775e3926707d0e8f256ecb8ca92bbaac59
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2025-11-20 18:16:07 +0530

    added error codes

---
+++ commcare_connect/utils/error_codes.py
@@ -0,0 +1,7 @@
+class ErrorCodes:
+    OPPORTUNITY_REQUIRED = "opportunity_required"
+    FAILED_USER_CREATE = "failed_user_create"
+    NO_OPPORTUNITY_ACCESS = "no_opportunity_access"
+    OPPORTUNITY_FULL = "opportunity_full"
+    OPPORTUNITY_ENDED = "opportunity_ended"
+    INVALID_FLAG = "invalid_flag"
