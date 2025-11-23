d1fdc13332651f250d21b6c53fb8d1dfd866ee23
Ajeet <ayadav@dimagi.com>
2025-11-23 07:08:26 +0530

    use send_mail_async task for send_org_invite email

--- commcare_connect/organization/tasks.py
+++ commcare_connect/organization/tasks.py
@@ -1,14 +1,12 @@
 from allauth.utils import build_absolute_uri
-from django.conf import settings
-from django.core.mail import send_mail
 from django.urls import reverse
 
 from commcare_connect.organization.models import UserOrganizationMembership
 from commcare_connect.users.models import User
+from commcare_connect.utils.tasks import send_mail_async
 from config import celery_app
 
 
-@celery_app.task()
 def send_org_invite(membership_id, host_user_id):
     membership = UserOrganizationMembership.objects.get(pk=membership_id)
     host_user = User.objects.get(pk=host_user_id)
@@ -25,11 +23,10 @@ The invite can be accepted by visiting the link.
 
 Thank You,
 Commcare Connect"""
-    send_mail(
+    send_mail_async(
         subject=f"{host_user.name} has invite you to join '{membership.organization.name}' on CommCare Connect",
         message=message,
         recipient_list=[membership.user.email],
-        from_email=settings.DEFAULT_FROM_EMAIL,
     )
 
 
