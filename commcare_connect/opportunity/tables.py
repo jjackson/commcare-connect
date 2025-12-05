c31f451abfea2d08c5edfd3f41741c0669bc37db
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-12-05 13:20:11 +0200

    add review button

--- commcare_connect/opportunity/tables.py
+++ commcare_connect/opportunity/tables.py
@@ -372,7 +372,7 @@ class PaymentInvoiceTable(OpportunityContextTable):
     amount = tables.Column(verbose_name="Amount")
     payment_status = columns.Column(verbose_name="Payment Status", accessor="payment", empty_values=())
     payment_date = columns.Column(verbose_name="Payment Date", accessor="payment", empty_values=(None))
-    actions = tables.Column(empty_values=(), orderable=False, verbose_name="Pay")
+    actions = tables.Column(empty_values=(), orderable=False, verbose_name="Actions")
     exchange_rate = tables.Column(orderable=False, empty_values=(None,), accessor="exchange_rate__rate")
     amount_usd = tables.Column(verbose_name="Amount (USD)")
 
@@ -396,6 +396,7 @@ class PaymentInvoiceTable(OpportunityContextTable):
     def __init__(self, *args, **kwargs):
         self.csrf_token = kwargs.pop("csrf_token")
         self.opportunity = kwargs.pop("opportunity")
+        self.is_pm = kwargs.pop("is_pm", False)
         super().__init__(*args, **kwargs)
         self.base_columns["amount"].verbose_name = f"Amount ({self.opportunity.currency})"
 
@@ -410,17 +411,23 @@ class PaymentInvoiceTable(OpportunityContextTable):
         return
 
     def render_actions(self, record):
-        invoice_approve_url = reverse("opportunity:invoice_approve", args=[self.org_slug, self.opportunity.id])
-        disabled = "disabled" if getattr(record, "payment", None) else ""
-        template_string = f"""
-            <form method="POST" action="{ invoice_approve_url  }">
-                <input type="hidden" name="csrfmiddlewaretoken" value="{ self.csrf_token }">
-                <input type="hidden" name="pk" value="{ record.pk }">
-                <button type="submit" class="button button-md outline-style" { disabled }>
-                Pay</button>
-            </form>
-        """  # noqa: E501
-        return mark_safe(template_string)
+        invoice_review_url = reverse(
+            "opportunity:invoice_review", args=[self.org_slug, self.opportunity.id, record.pk]
+        )
+        review_button = f'<a href="{invoice_review_url}" class="button button-md outline-style">{_("Review")}</a>'
+        pay_button = ""
+        if self.is_pm:
+            invoice_approve_url = reverse("opportunity:invoice_approve", args=[self.org_slug, self.opportunity.id])
+            disabled = "disabled" if getattr(record, "payment", None) else ""
+            pay_button = f"""
+                <form method="POST" action="{invoice_approve_url}" class="inline">
+                    <input type="hidden" name="csrfmiddlewaretoken" value="{self.csrf_token}">
+                    <input type="hidden" name="pk" value="{record.pk}">
+                    <button type="submit" class="button button-md outline-style" {disabled}>
+                    {_("Pay")}</button>
+                </form>
+            """  # noqa: E501
+        return mark_safe(f'<div class="flex gap-2">{review_button}{pay_button}</div>')
 
 
 def popup_html(value, popup_title, popup_direction="top", popup_class="", popup_attributes=""):
