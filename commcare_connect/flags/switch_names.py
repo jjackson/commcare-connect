f3493c685c476d43ed90f340c0f47215222d57ea
Ajeet <ayadav@dimagi.com>
2025-12-09 10:10:51 +0530

    add  a new switch for automated invoice monthly generation

--- commcare_connect/flags/switch_names.py
+++ commcare_connect/flags/switch_names.py
@@ -1,3 +1,4 @@
 OPPORTUNITY_CREDENTIALS = "opportunity_credentials"
 AUTOMATED_INVOICES = "automated_invoices"
 INVOICE_REVIEW = "invoice_review"
+AUTOMATED_INVOICES_MONTHLY = "automated_invoices_monthly"
