1cc9ab19d7765bd0a16a904ca43a3fe6f3ef71c2
Charl Smit <csmit@dimagi.com>
2025-12-11 12:45:21 +0200

    Update location flags specification

--- commcare_connect/opportunity/views.py
+++ commcare_connect/opportunity/views.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import sys
 from collections import Counter, defaultdict
 from datetime import timedelta
 from decimal import Decimal, InvalidOperation
@@ -1976,7 +1975,6 @@ def user_visit_details(request, org_slug, opp_id, pk):
 
     user_forms = []
     other_forms = []
-    closest_distance = sys.maxsize
 
     if user_visit.location:
         lat, lon, _, precision = user_visit.location.split(" ")
@@ -2012,7 +2010,6 @@ def user_visit_details(request, org_slug, opp_id, pk):
             try:
                 other_lat, other_lon, *_ = loc.location.split()
                 dist = distance.distance((lat, lon), (float(other_lat), float(other_lon))).m
-                closest_distance = int(min(closest_distance, dist))
                 if dist <= 250:
                     visit_info = {
                         "entity_name": loc.entity_name,
@@ -2055,7 +2052,7 @@ def user_visit_details(request, org_slug, opp_id, pk):
             user_forms=user_forms[:5],
             other_forms=other_forms[:5],
             visit_data=visit_data,
-            closest_distance=closest_distance,
+            min_allowed_distance=verification_flags_config.location,
             verification_flags_config=verification_flags_config,
             flags=flags,
             flag_count=flag_count,
