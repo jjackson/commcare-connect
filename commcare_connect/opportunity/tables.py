6050470d50299f37e9ee25a3025740af4d6d12ad
Charl Smit <csmit@dimagi.com>
2025-12-17 11:14:58 +0200

    Fix table

--- commcare_connect/opportunity/tables.py
+++ commcare_connect/opportunity/tables.py
@@ -378,12 +378,12 @@ class PaymentInvoiceTable(OpportunityContextTable):
     actions = tables.Column(empty_values=(), orderable=False, verbose_name="Actions")
     exchange_rate = tables.Column(orderable=False, empty_values=(None,), accessor="exchange_rate__rate")
     amount_usd = tables.Column(verbose_name="Amount (USD)")
-    invoice_type = tables.Column(verbose_name="Invoice Type", accessor="service_delivery")
+    invoice_type = tables.Column(verbose_name="Invoice Type", accessor="service_delivery", empty_values=())
 
     class Meta:
         model = PaymentInvoice
         orderable = False
-        fields = ("amount", "date", "invoice_number", "service_delivery")
+        fields = ("amount", "date", "invoice_number")
         sequence = (
             "amount",
             "amount_usd",
@@ -420,8 +420,8 @@ class PaymentInvoiceTable(OpportunityContextTable):
             return value.date_paid
         return
 
-    def render_invoice_type(self, value):
-        if value == PaymentInvoice.InvoiceType.service_delivery:
+    def render_invoice_type(self, record):
+        if record.service_delivery:
             return _("Service Delivery")
         return _("Other")
 
