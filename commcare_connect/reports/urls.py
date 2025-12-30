f35fa726ac80a6ae35e9857e925404a65862652e
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2025-12-30 22:45:20 +0530

    added url and view for the report

--- commcare_connect/reports/urls.py
+++ commcare_connect/reports/urls.py
@@ -6,4 +6,5 @@ app_name = "reports"
 
 urlpatterns = [
     path("delivery_stats", view=views.DeliveryStatsReportView.as_view(), name="delivery_stats_report"),
+    path("invoice_report", view=views.InvoiceReportView.as_view(), name="invoice_report"),
 ]
