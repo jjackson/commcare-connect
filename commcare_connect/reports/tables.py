3fc7807b7e8790a71475adc7ef7d2e15a929be6c
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2026-01-05 14:14:55 +0530

    added table method to avoid use of rander

--- commcare_connect/reports/tables.py
+++ commcare_connect/reports/tables.py
@@ -87,6 +87,9 @@ class InvoiceReportTable(tables.Table):
             value,
         )
 
+    def value_invoice_number(self, value, record):
+        return value
+
     def render_amount(self, record):
         return f"{record.opportunity.currency} {record.amount}"
 
