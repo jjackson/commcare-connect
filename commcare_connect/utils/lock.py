3a0690faa254bd3dcb1cd22717e77d61cc8ab925
Sravan Reddy <sravfeyn@gmail.com>
2026-01-12 11:48:11 +0530

    move concurrency logic behind a flag

--- commcare_connect/utils/lock.py
+++ commcare_connect/utils/lock.py
@@ -3,10 +3,17 @@ from contextlib import contextmanager
 import sentry_sdk
 from django.core.cache import cache
 from redis.exceptions import LockError
+from waffle import switch_is_active
+
+from commcare_connect.flags.switch_names import CONCURRENT_SUBMISSIONS_LOCK
 
 
 @contextmanager
 def try_redis_lock(key: str, *, timeout: int = 10, blocking_timeout: float = 0, sleep: float = 0.1):
+    if not switch_is_active(CONCURRENT_SUBMISSIONS_LOCK):
+        yield True
+        return
+
     lock = cache.lock(key, timeout=timeout, sleep=sleep)
     acquired = False
     try:
