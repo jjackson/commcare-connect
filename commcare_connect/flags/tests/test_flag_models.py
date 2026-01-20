1807609870473dba150df99bd38d3e1e6c14c7fa
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2026-01-20 14:52:37 +0200

    check if user exists in enabled opp, org, or program flag

--- commcare_connect/flags/tests/test_flag_models.py
+++ commcare_connect/flags/tests/test_flag_models.py
@@ -3,9 +3,9 @@ from django.core.cache import cache
 
 from commcare_connect.flags.models import Flag
 from commcare_connect.flags.tests.factories import FlagFactory
-from commcare_connect.opportunity.tests.factories import OpportunityFactory
+from commcare_connect.opportunity.tests.factories import OpportunityAccessFactory, OpportunityFactory
 from commcare_connect.program.tests.factories import ProgramFactory
-from commcare_connect.users.tests.factories import UserFactory
+from commcare_connect.users.tests.factories import MembershipFactory, OrganizationFactory, UserFactory
 
 
 @pytest.fixture
@@ -52,12 +52,38 @@ class TestFlagModel:
         assert active_flags.count() == 1
         assert active_flags[0] == user_flag
 
+    def test_active_flags_for_user_segments(self):
+        user = UserFactory()
+
+        organization = OrganizationFactory()
+        MembershipFactory(user=user, organization=organization)
+
+        opportunity = OpportunityFactory()
+        OpportunityAccessFactory(user=user, opportunity=opportunity)
+
+        program = ProgramFactory(organization=organization)
+
+        org_flag = FlagFactory()
+        org_flag.organizations.add(organization)
+
+        opportunity_flag = FlagFactory()
+        opportunity_flag.opportunities.add(opportunity)
+
+        program_flag = FlagFactory()
+        program_flag.programs.add(program)
+
+        FlagFactory()  # unrelated to user
+
+        active_flags = Flag.active_flags_for_user(user)
+        assert active_flags.count() == 3
+        assert set(active_flags) == {org_flag, opportunity_flag, program_flag}
+
     def test_active_flags_for_user_role_flags(self):
         user = UserFactory(is_staff=True)
         staff_flag = FlagFactory(staff=True)
         everyone_flag = FlagFactory(everyone=True)
         FlagFactory(superusers=True)
-        FlagFactory()
+        FlagFactory()  # unrelated to user
 
         active_flags = Flag.active_flags_for_user(user, include_role_flags=True)
         assert active_flags.count() == 2
