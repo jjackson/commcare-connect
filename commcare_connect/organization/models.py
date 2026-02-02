baeb42e5bba26d85cf367c51cfbfb93f180ea116
Pawan Verma <pverma@dimagi.com>
2026-02-02 13:43:42 +0530

    Add LLO Entity model

--- commcare_connect/organization/models.py
+++ commcare_connect/organization/models.py
@@ -8,6 +8,10 @@ from commcare_connect.users.models import User
 from commcare_connect.utils.db import BaseModel, slugify_uniquely
 
 
+class LLOEntity(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Organization(BaseModel):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
@@ -15,6 +19,7 @@ class Organization(BaseModel):
         settings.AUTH_USER_MODEL, related_name="organizations", through="UserOrganizationMembership"
     )
     program_manager = models.BooleanField(default=False)
+    llo_entity = models.ForeignKey(LLOEntity, on_delete=models.SET_NULL, null=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
