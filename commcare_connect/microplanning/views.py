87420e7e409d20a49fd934788fe7785fe8f0bfa7
Charl Smit <csmit@dimagi.com>
2026-02-04 10:56:00 +0200

    Restrict migroplanning page to admins

--- commcare_connect/microplanning/views.py
+++ commcare_connect/microplanning/views.py
@@ -6,11 +6,11 @@ from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_GET
 
 from commcare_connect.flags.switch_names import MICROPLANNING
-from commcare_connect.organization.decorators import org_viewer_required
+from commcare_connect.organization.decorators import org_admin_required
 
 
 @require_GET
-@org_viewer_required
+@org_admin_required
 def microplanning_home(request, *args, **kwargs):
     if not waffle.switch_is_active(MICROPLANNING):
         raise Http404(_("Microplanning feature is not available"))
