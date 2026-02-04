3d75edbf69526cc99b3ad124ce0d266403159644
Charl Smit <csmit@dimagi.com>
2026-02-04 11:53:43 +0200

    Add microplanning behind FF instead of switch

---
+++ commcare_connect/flags/flag_names.py
@@ -0,0 +1,1 @@
+MICROPLANNING = "microplanning"
--- commcare_connect/microplanning/views.py
+++ commcare_connect/microplanning/views.py
@@ -1,20 +1,16 @@
-import waffle
 from django.conf import settings
-from django.http import Http404
 from django.shortcuts import render
-from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_GET
 
-from commcare_connect.flags.switch_names import MICROPLANNING
+from commcare_connect.flags.decorators import require_flag_for_org
+from commcare_connect.flags.flag_names import MICROPLANNING
 from commcare_connect.organization.decorators import org_admin_required
 
 
 @require_GET
 @org_admin_required
+@require_flag_for_org(MICROPLANNING)
 def microplanning_home(request, *args, **kwargs):
-    if not waffle.switch_is_active(MICROPLANNING):
-        raise Http404(_("Microplanning feature is not available"))
-
     return render(
         request,
         template_name="microplanning/home.html",
