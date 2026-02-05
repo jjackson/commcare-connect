92f850a167664880d74c3c7cad463faa8f5e7932
Pawan Verma <pverma@dimagi.com>
2026-02-05 17:25:49 +0530

    Add tests

--- commcare_connect/organization/tests/test_forms.py
+++ commcare_connect/organization/tests/test_forms.py
@@ -1,9 +1,13 @@
 import pytest
+from django.contrib.auth.models import Permission
 from django.test import Client
 from django.urls import reverse
 
-from commcare_connect.organization.models import Organization
+from commcare_connect.organization.forms import OrganizationChangeForm
+from commcare_connect.organization.models import LLOEntity, Organization
+from commcare_connect.users.models import User
 from commcare_connect.users.tests.factories import UserFactory
+from commcare_connect.utils.permission_const import WORKSPACE_ENTITY_MANAGEMENT_ACCESS
 
 
 class TestAddMembersView:
@@ -52,3 +56,89 @@ class TestAddMembersView:
         if should_exist and expected_role:
             membership = organization.memberships.get(**membership_filter)
             assert membership.role == expected_role
+
+
+@pytest.mark.django_db
+class TestOrganizationChangeForm:
+    def test_update_name(self, organization: Organization):
+        user = UserFactory()
+        form = OrganizationChangeForm(data={"name": "New Name"}, user=user, instance=organization)
+        assert form.is_valid()
+        form.save()
+        organization.refresh_from_db()
+        assert organization.name == "New Name"
+
+    def test_update_program_manager_without_permission(self, organization: Organization):
+        user = UserFactory()
+        organization.program_manager = False
+        organization.save()
+        form = OrganizationChangeForm(
+            data={"name": "New Name", "program_manager": True},
+            user=user,
+            instance=organization,
+        )
+        assert form.is_valid()
+        form.save()
+        organization.refresh_from_db()
+        assert not organization.program_manager
+        assert organization.name == "New Name"
+
+    def test_update_llo_entity_with_permission(self, organization: Organization):
+        user = UserFactory()
+        app_label, codename = WORKSPACE_ENTITY_MANAGEMENT_ACCESS.split(".")
+        perm = Permission.objects.get(codename=codename, content_type__app_label=app_label)
+        user.user_permissions.add(perm)
+        user = User.objects.get(pk=user.pk)
+        llo_entity = LLOEntity.objects.create(name="Test LLO")
+        organization.llo_entity = None
+        organization.save()
+
+        form = OrganizationChangeForm(
+            data={"name": organization.name, "llo_entity": llo_entity.pk},
+            user=user,
+            instance=organization,
+        )
+        assert form.is_valid(), form.errors
+        form.save()
+        organization.refresh_from_db()
+        assert organization.llo_entity == llo_entity
+
+    def test_create_llo_entity_with_permission(self, organization: Organization):
+        user = UserFactory()
+        app_label, codename = WORKSPACE_ENTITY_MANAGEMENT_ACCESS.split(".")
+        perm = Permission.objects.get(codename=codename, content_type__app_label=app_label)
+        user.user_permissions.add(perm)
+        user = User.objects.get(pk=user.pk)
+
+        organization.llo_entity = None
+        organization.save()
+
+        assert LLOEntity.objects.count() == 0
+        form = OrganizationChangeForm(
+            data={"name": organization.name, "llo_entity": "New LLO Entity"},
+            user=user,
+            instance=organization,
+        )
+        assert form.is_valid(), form.errors
+        form.save()
+        organization.refresh_from_db()
+        assert organization.llo_entity is not None
+        assert organization.llo_entity.name == "New LLO Entity"
+        assert LLOEntity.objects.count() == 1
+
+    def test_update_llo_entity_without_permission(self, organization: Organization):
+        user = UserFactory()
+        llo_entity = LLOEntity.objects.create(name="Test LLO")
+        organization.llo_entity = None
+        organization.save()
+
+        form = OrganizationChangeForm(
+            data={"name": "New Name", "llo_entity": llo_entity.pk},
+            user=user,
+            instance=organization,
+        )
+        assert form.is_valid()
+        form.save()
+        organization.refresh_from_db()
+        assert organization.llo_entity is None
+        assert organization.name == "New Name"
