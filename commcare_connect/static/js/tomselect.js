205326037a65a841ce2bf708aac16d8d8f04ec59
Pawan Verma <pverma@dimagi.com>
2026-02-09 13:58:12 +0530

    Add custom create text

--- commcare_connect/static/js/tomselect.js
+++ commcare_connect/static/js/tomselect.js
@@ -2,8 +2,11 @@ import 'tom-select/dist/css/tom-select.min.css';
 import '../css/tomselect-overrides.css';
 import TomSelect from 'tom-select';
 
+window.TomSelect = TomSelect;
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('[data-tomselect]').forEach((el) => {
+    if (el.tomselect) return;
     el.removeAttribute('class');
     let plugins = [];
     if (!el.hasAttribute('data-tomselect:no-remove-button')) {
