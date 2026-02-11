32ddec057157fe17fe070f1ec12fde614aa2b0f5
Pawan Verma <pverma@dimagi.com>
2026-02-11 13:43:46 +0530

    Change payment text

--- commcare_connect/opportunity/tasks.py
+++ commcare_connect/opportunity/tasks.py
@@ -321,7 +321,7 @@ def send_payment_notification(opportunity_id: int, payment_ids: list[int]):
                     "opportunity_id": str(opportunity.id),
                     "opportunity_uuid": str(opportunity.opportunity_id),
                     "title": gettext("Payment received"),
-                    "body": gettext(f"You have received a payment for {opportunity.name} Opportunity - see how much!"),
+                    "body": gettext(f"A payment is now available for {opportunity.name} Opportunity."),
                     "payment_id": str(payment.id),
                     "payment_uuid": str(payment.payment_id),
                 },
