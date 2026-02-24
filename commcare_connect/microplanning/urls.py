8076d93361039aa0b4e10a9615d08200bc1c84ad
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2026-02-24 15:56:53 +0530

    added urls

--- commcare_connect/microplanning/urls.py
+++ commcare_connect/microplanning/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<slug:opp_id>/", view=views.microplanning_home, name="microplanning_home"),
     path("<slug:opp_id>/upload_work_areas/", views.WorkAreaImport.as_view(), name="upload_work_areas"),
     path("<slug:opp_id>/import_status/", views.import_status, name="import_status"),
+    path("<slug:opp_id>/workareas_geojson/", views.workareas_geojson, name="workareas_geojson"),
 ]
