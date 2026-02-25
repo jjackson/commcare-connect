0a5a4c6ed771b2e01e5bae5384e8b97fdf84dab9
Charl Smit <csmit@dimagi.com>
2026-02-25 13:53:48 +0200

    Use crispy forms for layout

--- commcare_connect/flags/forms.py
+++ commcare_connect/flags/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Column, Field, Layout, Row, Submit
 from django import forms
 from django.contrib.auth import get_user_model
 
@@ -38,3 +40,21 @@ class FlagForm(forms.ModelForm):
     class Meta:
         model = Flag
         fields = ["users", "organizations", "programs", "opportunities"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Column(Field("users"), css_class="col-span-1"),
+                Column(Field("organizations"), css_class="col-span-1"),
+                Column(Field("programs"), css_class="col-span-1"),
+                Column(Field("opportunities"), css_class="col-span-1"),
+                css_class="grid grid-cols-1 sm:grid-cols-2 gap-4",
+            ),
+            Row(
+                Submit("submit", "Save", css_class="button button-md primary-dark !inline-flex items-center gap-2"),
+                css_class="flex justify-end mt-3",
+            ),
+        )
