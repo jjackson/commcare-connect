88a8267142b58b87eea8151115a53c555fdb43e5
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2026-03-04 16:45:06 +0200

    add fields to task model

--- commcare_connect/opportunity/models.py
+++ commcare_connect/opportunity/models.py
@@ -261,14 +261,18 @@ class LearnModule(models.Model):
 class Task(models.Model):
     app = models.ForeignKey(CommCareApp, on_delete=models.CASCADE, related_name="tasks")
     slug = models.SlugField()
+    task_unit_name = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    case_property = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        constraints = [models.UniqueConstraint(fields=["app_id", "slug"], name="unique_task_per_app")]
+        constraints = [
+            models.UniqueConstraint(fields=["app_id", "slug"], name="unique_task_per_app"),
+        ]
 
 
 class XFormBaseModel(models.Model):
