2d109dd35a4cc2110cea9314367bf1227ac94c64
Simon Kelly <skelly@dimagi.com>
2026-05-25 10:21:09 +0200

    Add 'email' field to UserSerializer

--- commcare_connect/users/api/serializers.py
+++ commcare_connect/users/api/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["name", "url"]
+        fields = ["email", "name", "url"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
